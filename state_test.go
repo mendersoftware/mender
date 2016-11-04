7e02d486a106056d3477d1b90331f6bb3cd2c961
Maciej Borzecki <maciej.borzecki@rndity.com>
2016-11-04 15:08:40 +0100

    state_test: fix nil dereference if TestStateUpdateInstall was run separately
    Signed-off-by: Maciej Borzecki <maciej.borzecki@rndity.com>

--- state_test.go
+++ state_test.go
@@ -747,6 +747,11 @@ func TestStateUpdateInstall(t *testing.T) {
 	}
 	uis := NewUpdateInstallState(stream, int64(len(data)), update)
 
+	// create directory for storing deployments logs
+	tempDir, _ := ioutil.TempDir("", "logs")
+	defer os.RemoveAll(tempDir)
+	DeploymentLogger = NewDeploymentLogManager(tempDir)
+
 	ms := NewMemStore()
 	ctx := StateContext{
 		store: ms,
