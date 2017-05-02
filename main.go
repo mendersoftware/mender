41e8899cd2893f25e7041200ac6ee6b25a4608bf
Greg Di Stefano <greg.distefano+github@gmail.com>
2017-05-02 16:34:15 +0200

    test
    Signed-off-by: Greg Di Stefano <greg.distefano+github@gmail.com>

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// Copyright 2016 Mender Software AS
+// TESTCopyright 2016 Mender Software AS
 //
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
