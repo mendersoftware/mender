eb8775eba6a31d7008c27c6947f193771edd6342
Marcin Pasinski <marcin.pasinski@mender.io>
2017-05-26 13:38:19 +0200

    Add new IdleState and IsAuthorized() controller method.
    Changelog: None
    
    Signed-off-by: Marcin Pasinski <marcin.pasinski@mender.io>

--- mender.go
+++ mender.go
@@ -48,6 +48,7 @@ type UInstallCommitRebooter interface {
 }
 
 type Controller interface {
+	IsAuthorized() bool
 	Authorize() menderError
 	Bootstrap() menderError
 	GetCurrentArtifactName() (string, error)
@@ -82,6 +83,8 @@ type MenderState int
 const (
 	// initial state
 	MenderStateInit MenderState = iota
+	// idle state; waiting for transition to the new state
+	MenderStateIdle
 	// client is bootstrapped, i.e. ready to go
 	MenderStateBootstrapped
 	// client has all authorization data available
@@ -126,6 +129,7 @@ const (
 var (
 	stateNames = map[MenderState]string{
 		MenderStateInit:                  "init",
+		MenderStateIdle:                  "idle",
 		MenderStateBootstrapped:          "bootstrapped",
 		MenderStateAuthorized:            "authorized",
 		MenderStateAuthorizeWait:         "authorize-wait",
@@ -321,6 +325,10 @@ func (m *mender) loadAuth() menderError {
 
 	m.authToken = code
 	return nil
+}
+
+func (m *mender) IsAuthorized() bool {
+	return m.authMgr.IsAuthorized()
 }
 
 func (m *mender) Authorize() menderError {
