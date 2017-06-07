15b76b89c6460c923f8aee171475af4a5655a060
Marcin Pasinski <marcin.pasinski@mender.io>
2017-06-07 11:07:55 +0200

    MEN-1191 Add state script storing functionality.
    While reading artifact containing the scripts those must be stored
    for further use.
    
    Changelog: None
    
    Signed-off-by: Marcin Pasinski <marcin.pasinski@mender.io>

--- mender.go
+++ mender.go
@@ -72,6 +72,7 @@ var (
 	defaultArtifactInfoFile = path.Join(getConfDirPath(), "artifact_info")
 	defaultDeviceTypeFile   = path.Join(getStateDirPath(), "device_type")
 	defaultDataStore        = getStateDirPath()
+	defaultArtScriptsPath    = path.Join(getStateDirPath(), "scripts")
 )
 
 type MenderState int
@@ -181,6 +182,7 @@ type mender struct {
 	authMgr          AuthManager
 	api              *client.ApiClient
 	authToken        client.AuthToken
+	stateScriptPath     string
 }
 
 type MenderPieces struct {
@@ -206,6 +208,7 @@ func NewMender(config menderConfig, pieces MenderPieces) (*mender, error) {
 		authReq:                client.NewAuth(),
 		api:                    api,
 		authToken:              noAuthToken,
+		stateScriptPath:        defaultArtScriptsPath,
 	}
 	return m, nil
 }
@@ -521,5 +524,5 @@ func (m *mender) InstallUpdate(from io.ReadCloser, size int64) error {
 		log.Errorf("Unable to verify the existing hardware. Update will continue anyways: %v : %v", defaultDeviceTypeFile, err)
 	}
 	return installer.Install(from, deviceType,
-		m.GetArtifactVerifyKey(), m.UInstallCommitRebooter)
+		m.GetArtifactVerifyKey(), m.stateScriptPath, m.UInstallCommitRebooter)
 }
