d7064c82c66cb7e4093c71c44b2c976c0caac8a2
Angelo Compagnucci <angelo@amarulasolutions.com>
2018-08-09 09:28:06 +0200

    FIX: Enabling compiling ppc64le
    This patch enables Mender compilation on ppc64le
    
    Changelog: FIX: Enabling compiling ppc64le
    
    Signed-off-by: Angelo Compagnucci <angelo@amarulasolutions.com>
    (cherry picked from commit 2b13f9a50793fdc15199925000107cb0cc4e7bad)

--- ioctl_64_bit.go
+++ ioctl_64_bit.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Northern.tech AS
+// Copyright 2018 Northern.tech AS
 //
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 
-// +build amd64 arm64
+// +build amd64 arm64 ppc64le
 
 package main
 
