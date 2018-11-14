e3b8ef7911d93d3f4a787a7401b175b037c18b13
Kristian Amlie <kristian.amlie@northern.tech>
2018-11-14 08:34:02 +0100

    Fix various printf formatting mistakes found by Yocto builder.
    Changelog: None
    
    Signed-off-by: Kristian Amlie <kristian.amlie@northern.tech>
    (cherry picked from commit a9c77982d38599ecba36093951b5c63250f682f4)
    
    Conflicts:
    	config.go
    	device.go

--- device.go
+++ device.go
@@ -54,7 +54,7 @@ func NewDevice(env BootEnvReadWriter, sc StatCommander, config deviceConfig) *de
 }
 
 func (d *device) Reboot() error {
-	log.Info("Mender rebooting from active partition: %s", d.active)
+	log.Infof("Mender rebooting from active partition: %s", d.active)
 	return d.Command("reboot").Run()
 }
 
