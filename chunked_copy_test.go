4892f9d62af156275b92f116e3b93a665812dfc1
Jeremy Trimble <jeremy.trimble@twosixlabs.com>
2018-12-14 14:16:44 -0500

    MEN-2285: device.go: Do "medium-sized" writes rather than individual sectors:
    Testing showed that writing to the block device in increments of single
    sectors (512 bytes on my eMMC device) contributed to an OOM condition in
    the kernel when allocating DMA scatter-gather descriptors.
    
    Rather than making a zillion small writes and expecting the kernel to
    coalesce them for us when we're easily able put this data into already
    (virtually) contiguous memory in userspace, we write to the block device
    in "medium-size chunks" (around 1 MiB) that are a power-of-two multiple
    of the sector size (obviating any concerns about sector misalignment
    causing reduced performance or undue wear on the underlying storage).
    
    The 1 MiB size could be made configurable if need be.
    
    This commit introduces the following:
    
    - Write to block device at some power-of-two multiple of the sector size.
    - We introduce chunkedCopy() which ensures that writes happen exactly in
      increments of the specified size (except for the last write after EOF
      of the input).  Added unit test to verify correct behavior.
    
    Changelog: Work around occasional OOM bug in mmc driver.
    Signed-off-by: Jeremy Trimble <jeremy.trimble@twosixlabs.com>

---
+++ chunked_copy_test.go
@@ -0,0 +1,77 @@
+// Copyright 2018 Northern.tech AS
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"testing"
+)
+
+type MyWriter struct {
+	writtenChunkSizes []int
+	writtenData       bytes.Buffer
+}
+
+func (mw *MyWriter) Write(data []byte) (n int, err error) {
+	mw.writtenChunkSizes = append(mw.writtenChunkSizes, len(data))
+	n, err = mw.writtenData.Write(data)
+	return
+}
+
+func (mw *MyWriter) GetTotalOutput() []byte {
+	return mw.writtenData.Bytes()
+
+}
+
+func TestChunkedCopy(t *testing.T) {
+
+	CHUNK_SIZE := int64(337)
+
+	bldr := strings.Builder{}
+
+	for i := 0; i < 1000; i++ {
+		bldr.WriteString(fmt.Sprintf("x%07d", i)) // can ignore return value
+	}
+
+	input_bytes := []byte(bldr.String())
+
+	mw := &MyWriter{}
+
+	bytes_written, err := chunkedCopy(mw, bytes.NewBuffer(input_bytes), CHUNK_SIZE) // use a strange chunk size just to prove it works
+
+	// a Copy() routine should return err == nil on EOF.
+	if err != nil {
+		t.Errorf("chunkedCopy: expected err to be nil but got: %v", err)
+	}
+
+	if int(bytes_written) != len(input_bytes) {
+		t.Errorf("chunkedCopy: did not copy full input: expected %v, got %v", len(input_bytes), bytes_written)
+	}
+
+	output_bytes := mw.GetTotalOutput()
+
+	if !bytes.Equal(output_bytes, input_bytes) {
+		t.Errorf("chunkedCopy: output did not match input")
+	}
+
+	// This is what we really want to check. All of the calls to Write (except the last) must be of size CHUNK_SIZE
+	for i := 0; i < len(mw.writtenChunkSizes)-2; i++ {
+		if mw.writtenChunkSizes[i] != int(CHUNK_SIZE) {
+			t.Errorf("chunkedCopy: writtenChunkSizes[%d] = %v, expected %v", i, mw.writtenChunkSizes[i], CHUNK_SIZE)
+		}
+	}
+
+}
