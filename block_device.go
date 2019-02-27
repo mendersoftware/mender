63d700f6dec44d37e522a5f0830e890c7a563bff
Jeremy Trimble <jeremy.trimble@twosixlabs.com>
2019-02-27 11:42:13 -0500

    block_device.go: Refactor into BlockDeviceFile, BlockDeviceWriter...
    ...add checkpointing support (periodic sync, progress callbacks).  Unit
    tests for new functionality.
    
    Changelog: None
    Signed-off-by: Jeremy Trimble <jeremy.trimble@twosixlabs.com>

--- block_device.go
+++ block_device.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Northern.tech AS
+// Copyright 2019 Northern.tech AS
 //
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
@@ -14,10 +14,14 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"os"
 
 	"github.com/mendersoftware/log"
 	"github.com/mendersoftware/mender/utils"
+	"github.com/pkg/errors"
 )
 
 var (
@@ -31,95 +35,108 @@ type BlockDeviceGetSizeFunc func(file *os.File) (uint64, error)
 // BlockDeviceGetSectorSizeFunc is a helper for obtaining the sector size of a block device.
 type BlockDeviceGetSectorSizeFunc func(file *os.File) (int, error)
 
-// BlockDevice is a low-level wrapper for a block device. The wrapper implements
+type ProgressCallback func(blockStartByte, blockEndByte int64)
+
+// BlockDeviceWriter is a low-level wrapper for a block device. The wrapper implements
 // io.Writer and io.Closer interfaces.
-type BlockDevice struct {
-	Path      string               // device path, ex. /dev/mmcblk0p1
-	out       *os.File             // os.File for writing
-	w         *utils.LimitedWriter // wrapper for `out` limited the number of bytes written
-	typeUBI   bool                 // Set to true if we are updating an UBI volume
-	ImageSize int64                // image size
+// BlockDeviceWriter writes to its underlying BlockDeviceFile using
+// medium-sized writes that are aligned with the underlying device's block
+// alignment (assumed to be a power of 2).  It periodically executes a Sync()
+// operation to ensure the data has been persisted to the underlying storage,
+// and then invokes the provided "progress" callback to indicate that a block
+// of bytes has been successfully persisted to stable storage. It can also be
+// Seek()-ed (as in io.Seeker), allowing data to be written starting "in the
+// middle" of the underlying BlockDeviceFile.
+
+type BlockDeviceWriter struct {
+	out                BlockDeviceFile
+	ImageSize          int64  // image size
+	FlushIntervalBytes uint64 // Force a flush to disk each time this many bytes are written
+	buf                *bytes.Buffer
+	progressCallback   ProgressCallback
 }
 
-// Write writes data `p` to underlying block device. Will automatically open
-// the device in a write mode. Otherwise, behaves like io.Writer.
-func (bd *BlockDevice) Write(p []byte) (int, error) {
-	if bd.out == nil {
-		log.Infof("opening device %s for writing", bd.Path)
-		out, err := os.OpenFile(bd.Path, os.O_WRONLY, 0)
-		if err != nil {
-			return 0, err
-		}
+// BlockDeviceFile is an interface that represents an underlying block device (storage volume).
+type BlockDeviceFile interface {
+	io.Writer
+	io.Closer
 
-		// From <mtd/ubi-user.h>
-		//
-		// UBI volume update
-		// ~~~~~~~~~~~~~~~~~
-		//
-		// Volume update should be done via the UBI_IOCVOLUP ioctl command of the
-		// corresponding UBI volume character device. A pointer to a 64-bit update
-		// size should be passed to the ioctl. After this, UBI expects user to write
-		// this number of bytes to the volume character device. The update is finished
-		// when the claimed number of bytes is passed. So, the volume update sequence
-		// is something like:
-		//
-		// fd = open("/dev/my_volume");
-		// ioctl(fd, UBI_IOCVOLUP, &image_size);
-		// write(fd, buf, image_size);
-		// close(fd);
-		if bd.typeUBI {
-			err := setUbiUpdateVolume(out, bd.ImageSize)
-			if err != nil {
-				log.Errorf("Failed to write images size to UBI_IOCVOLUP: %v", err)
-				return 0, err
-			}
-		}
+	Sync() error
+	Seek(offset int64, whence int) (int64, error)
 
-		size, err := BlockDeviceGetSizeOf(out)
-		if err != nil {
-			log.Errorf("failed to read block device size: %v", err)
-			out.Close()
-			return 0, err
-		}
-		log.Infof("partition %s size: %v", bd.Path, size)
+	Filepath() string
+	Size() (uint64, error)
+	SectorSize() (int, error)
+}
 
-		bd.out = out
-		bd.w = &utils.LimitedWriter{
-			W: out,
-			N: size,
-		}
-	}
+type basicDeviceFile struct {
+	*os.File
+	path string
+}
 
-	w, err := bd.w.Write(p)
+func newBasicDeviceFile(path string) (*basicDeviceFile, error) {
+	f, err := os.OpenFile(path, os.O_WRONLY, 0)
 	if err != nil {
-		log.Errorf("written %v out of %v bytes to partition %s: %v",
-			w, len(p), bd.Path, err)
+		return nil, err
 	}
-	return w, err
+
+	return &basicDeviceFile{
+		File: f,
+		path: path,
+	}, nil
 }
 
-// Close closes underlying block device automatically syncing any unwritten
-// data. Othewise, behaves like io.Closer.
-func (bd *BlockDevice) Close() error {
-	if bd.out != nil {
-		if err := bd.out.Sync(); err != nil {
-			log.Errorf("failed to fsync partition %s: %v", bd.Path, err)
-			return err
-		}
-		if err := bd.out.Close(); err != nil {
-			log.Errorf("failed to close partition %s: %v", bd.Path, err)
-		}
-		bd.out = nil
-		bd.w = nil
+type ubiDeviceFile struct {
+	basicDeviceFile
+	lw *utils.LimitedWriter
+}
+
+func newUBIDeviceFile(path string, ubiImageSize int64) (*ubiDeviceFile, error) {
+	f, err := os.OpenFile(path, os.O_WRONLY, 0)
+	if err != nil {
+		return nil, err
 	}
 
-	return nil
+	// This is UBI-specific stuff here.
+	// From <mtd/ubi-user.h>
+	//
+	// UBI volume update
+	// ~~~~~~~~~~~~~~~~~
+	//
+	// Volume update should be done via the UBI_IOCVOLUP ioctl command of the
+	// corresponding UBI volume character device. A pointer to a 64-bit update
+	// size should be passed to the ioctl. After this, UBI expects user to write
+	// this number of bytes to the volume character device. The update is finished
+	// when the claimed number of bytes is passed. So, the volume update sequence
+	// is something like:
+	//
+	// fd = open("/dev/my_volume");
+	// ioctl(fd, UBI_IOCVOLUP, &image_size);
+	// write(fd, buf, image_size);
+	// close(fd);
+	err = setUbiUpdateVolume(f, ubiImageSize)
+	if err != nil {
+		log.Errorf("Failed to write images size to UBI_IOCVOLUP: %v", err)
+		return nil, err
+	}
+
+	return &ubiDeviceFile{
+		basicDeviceFile: basicDeviceFile{
+			File: f,
+			path: path,
+		},
+		lw: &utils.LimitedWriter{f, uint64(ubiImageSize)},
+	}, nil
+}
+
+func (udf *ubiDeviceFile) Write(buf []byte) (int, error) {
+	return udf.lw.Write(buf)
 }
 
 // Size queries the size of the underlying block device. Automatically opens a
 // new fd in O_RDONLY mode, thus can be used in parallel to other operations.
-func (bd *BlockDevice) Size() (uint64, error) {
-	out, err := os.OpenFile(bd.Path, os.O_RDONLY, 0)
+func (bdf *basicDeviceFile) Size() (uint64, error) {
+	out, err := os.OpenFile(bdf.Filepath(), os.O_RDONLY, 0)
 	if err != nil {
 		return 0, err
 	}
@@ -130,12 +147,187 @@ func (bd *BlockDevice) Size() (uint64, error) {
 
 // SectorSize queries the logical sector size of the underlying block device. Automatically opens a
 // new fd in O_RDONLY mode, thus can be used in parallel to other operations.
-func (bd *BlockDevice) SectorSize() (int, error) {
-	out, err := os.OpenFile(bd.Path, os.O_RDONLY, 0)
+func (bdf *basicDeviceFile) SectorSize() (int, error) {
+	out, err := os.OpenFile(bdf.Filepath(), os.O_RDONLY, 0)
 	if err != nil {
 		return 0, err
 	}
 	defer out.Close()
 
 	return BlockDeviceGetSectorSizeOf(out)
+}
+
+func (bdf *basicDeviceFile) Filepath() string {
+	return bdf.path
+}
+
+func NewBlockDeviceWriter(
+	blockDeviceFile BlockDeviceFile,
+	imageSize int64,
+	chunkSize uint64, // Pass 0 to determine automatically
+	progressCallback ProgressCallback,
+) (*BlockDeviceWriter, error) {
+
+	bd := &BlockDeviceWriter{
+		out:                blockDeviceFile,
+		ImageSize:          imageSize,
+		FlushIntervalBytes: chunkSize,
+		progressCallback:   progressCallback,
+	}
+
+	if bd.FlushIntervalBytes < 1024 {
+		native_ssz, err := bd.SectorSize()
+		if err != nil {
+			return nil, err
+		}
+
+		chunk_size := uint64(native_ssz)
+
+		// Pick a multiple of the sector size that's around 1 MiB.
+		for chunk_size < 1*1024*1024 {
+			chunk_size = chunk_size * 2 // avoid doing logarithms...
+		}
+
+		bd.FlushIntervalBytes = chunk_size
+	}
+
+	return bd, nil
+}
+
+func (bd *BlockDeviceWriter) GetCurrentOffset() (int64, error) {
+	return bd.Seek(0, io.SeekCurrent) // seek to current position and report offset
+}
+
+func (bd *BlockDeviceWriter) Seek(offset int64, whence int) (int64, error) {
+	newOffset, err := bd.out.Seek(offset, whence)
+	return newOffset, err
+}
+
+func writeAll(b []byte, w io.Writer) (int, error) {
+	totalWritten := 0
+	for totalWritten < len(b) {
+		written, err := w.Write(b[totalWritten:])
+		totalWritten += written
+
+		if err != nil {
+			return totalWritten, err
+		}
+	}
+	return totalWritten, nil
+}
+
+// ReadFrom reads data from r and writes to the underlying block device. Behaves like io.ReaderFrom.
+func (bd *BlockDeviceWriter) ReadFrom(r io.Reader) (int64, error) {
+	totalBytesRead := int64(0)
+
+	currentOutputOffset, err := bd.GetCurrentOffset()
+	if err != nil {
+		return 0, errors.Wrapf(err, "unable to get current offset into device %s", bd.out.Filepath())
+	}
+
+	var remainingOutputBytes uint64
+	if currentOutputOffset < bd.ImageSize {
+		remainingOutputBytes = uint64(bd.ImageSize - currentOutputOffset)
+	}
+
+	if bd.buf == nil {
+		bd.buf = bytes.NewBuffer(make([]byte, 0, bd.FlushIntervalBytes))
+	}
+
+	chunkSizeBytes := int64(bd.FlushIntervalBytes)
+
+	for remainingOutputBytes > 0 {
+
+		// Note: bd.buf might already contain some bytes here
+		nextChunkBoundaryBytes := (currentOutputOffset + chunkSizeBytes) / chunkSizeBytes * chunkSizeBytes
+
+		thisChunkBytes := nextChunkBoundaryBytes - currentOutputOffset
+
+		bytesToRead := thisChunkBytes - int64(bd.buf.Len())
+
+		bytesRead, readErr := io.CopyN(bd.buf, r, bytesToRead)
+		totalBytesRead += bytesRead
+
+		shouldFlush := (readErr != nil) || (int64(bd.buf.Len()) == thisChunkBytes)
+
+		if shouldFlush {
+			// Time to write, flush, and sync, and report to callback.
+
+			bytesWritten, err := bd.buf.WriteTo(bd.out)
+			if err != nil {
+				return totalBytesRead, err
+			}
+
+			bd.buf.Reset() // after successful write
+
+			// Sync output to stable storage
+			err = bd.out.Sync()
+			if err != nil {
+				return totalBytesRead, err
+			}
+
+			//Invoke callback.
+			if bd.progressCallback != nil {
+				bd.progressCallback(currentOutputOffset, currentOutputOffset+bytesWritten)
+			}
+
+			// Update state
+			currentOutputOffset += bytesWritten
+		}
+
+		if readErr != nil {
+			if readErr == io.EOF {
+				readErr = nil
+			}
+			return totalBytesRead, readErr
+		}
+	}
+	return totalBytesRead, nil
+}
+
+func (bd *BlockDeviceWriter) CheckFullImageWritten() error {
+	if currentOffset, err := bd.GetCurrentOffset(); err != nil || currentOffset != bd.ImageSize {
+		if err != nil {
+			log.Errorf("failed to get current offset from device")
+			return err
+		} else {
+			msg := fmt.Sprintf("logic error: image size is %d but %d bytes were written to %s",
+				bd.ImageSize, currentOffset, bd.out.Filepath())
+			log.Error(msg)
+			return errors.Errorf("%s", msg)
+		}
+	}
+	return nil
+}
+
+// Close closes underlying block device automatically syncing any unwritten
+// data. Othewise, behaves like io.Closer.
+func (bd *BlockDeviceWriter) Close() error {
+
+	fullimgErr := bd.CheckFullImageWritten()
+
+	if bd.out != nil {
+		if err := bd.out.Sync(); err != nil {
+			log.Errorf("failed to fsync partition %s: %v", bd.out.Filepath(), err)
+			return err
+		}
+		if err := bd.out.Close(); err != nil {
+			log.Errorf("failed to close partition %s: %v", bd.out.Filepath(), err)
+		}
+		bd.out = nil
+	}
+
+	return fullimgErr
+}
+
+// Size queries the size of the underlying block device. Automatically opens a
+// new fd in O_RDONLY mode, thus can be used in parallel to other operations.
+func (bd *BlockDeviceWriter) Size() (uint64, error) {
+	return bd.out.Size()
+}
+
+// SectorSize queries the logical sector size of the underlying block device. Automatically opens a
+// new fd in O_RDONLY mode, thus can be used in parallel to other operations.
+func (bd *BlockDeviceWriter) SectorSize() (int, error) {
+	return bd.out.SectorSize()
 }
--- block_device_test.go
+++ block_device_test.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Northern.tech AS
+// Copyright 2019 Northern.tech AS
 //
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
@@ -14,124 +14,259 @@
 package main
 
 import (
-	"io/ioutil"
-	"os"
-	"path"
-	"syscall"
+	"fmt"
+	"io"
 	"testing"
 
-	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestBlockDeviceFail(t *testing.T) {
-	bd := BlockDevice{Path: "/dev/somefile"}
-
-	// closing unopened device should not fail
-	err := bd.Close()
-	assert.NoError(t, err)
-
-	w, err := bd.Write([]byte("foo"))
-	assert.Equal(t, 0, w)
+	_, err := newBasicDeviceFile("/dev/somefile")
 	assert.Error(t, err)
-
-	err = bd.Close()
-	assert.NoError(t, err)
 }
 
-func makeBlockDeviceSize(t *testing.T, sz uint64, err error, name string) BlockDeviceGetSizeFunc {
-	return func(file *os.File) (uint64, error) {
-		t.Logf("block device size called: %v", file)
-		if assert.NotNil(t, file) {
-			assert.Equal(t, name, file.Name())
-		}
-		return sz, err
-	}
+type MethodType int
+
+const Method_Write = 1
+const Method_Sync = 2
+const Method_Seek = 3
+const Method_Close = 4
+const Method_ProgressCallback = 5
+
+type Call struct {
+	methodType MethodType
+	writeData  []byte // valid if methodType = Method_Write
+
+	seekOffset int64 // valid of methodType = Method_Sync
+	seekWhence int   // valid of methodType = Method_Sync
+
+	progressBlockStartByte, progressBlockEndByte int64
+}
+type CallRecorder struct {
+	Calls      []Call
+	byteOffset int64
 }
 
-func createFile(path string) error {
-	f, err := os.Create(path)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
+func (cr *CallRecorder) Write(b []byte) (int, error) {
+	cr.Calls = append(cr.Calls, Call{
+		methodType: Method_Write,
+		writeData:  defensiveCopy(b),
+	})
+	cr.byteOffset += int64(len(b))
+	return len(b), nil
+}
+
+func defensiveCopy(p []byte) []byte {
+	rv := make([]byte, len(p))
+	copy(rv, p)
+	return rv
+}
+
+func (cr *CallRecorder) Sync() error {
+	cr.Calls = append(cr.Calls, Call{
+		methodType: Method_Sync,
+	})
 	return nil
 }
 
-func TestBlockDeviceWrite(t *testing.T) {
-	td, err := ioutil.TempDir("", "mender-block-device-")
-	assert.NoError(t, err)
-	defer os.RemoveAll(td)
+func (cr *CallRecorder) Seek(offset int64, whence int) (int64, error) {
+	cr.Calls = append(cr.Calls, Call{
+		methodType: Method_Seek,
+		seekOffset: offset,
+		seekWhence: whence,
+	})
 
-	// prepare fake block device file
-	bdpath := path.Join(td, "foo")
+	if whence == io.SeekStart {
+		cr.byteOffset = offset
+	} else if whence == io.SeekCurrent {
+		cr.byteOffset += offset
+	} else {
+		panic("not implemented")
+	}
 
-	// temporarily override helper for getting block device size
-	old := BlockDeviceGetSizeOf
-
-	// pretend the device is only 10 bytes in size
-	BlockDeviceGetSizeOf = makeBlockDeviceSize(t, 10, nil, bdpath)
-
-	// test simple write
-	err = createFile(bdpath)
-	assert.NoError(t, err)
-	bd := BlockDevice{Path: bdpath}
-	n, err := bd.Write([]byte("foobar"))
-	assert.Equal(t, len([]byte("foobar")), n)
-	assert.NoError(t, err)
-	bd.Close()
-
-	data, err := ioutil.ReadFile(bdpath)
-	assert.NoError(t, err)
-	assert.Equal(t, []byte("foobar"), data)
-
-	os.Remove(bdpath)
-
-	// too large write
-	err = createFile(bdpath)
-	assert.NoError(t, err)
-	bd = BlockDevice{Path: bdpath}
-	n, err = bd.Write([]byte("foobarfoobar"))
-	assert.Equal(t, 10, n)
-	assert.EqualError(t, err, syscall.ENOSPC.Error())
-	bd.Close()
-
-	data, err = ioutil.ReadFile(bdpath)
-	assert.NoError(t, err)
-	// written only 10 bytes
-	assert.Equal(t, []byte("foobarfoob"), data)
-
-	os.Remove(bdpath)
-
-	BlockDeviceGetSizeOf = old
+	return cr.byteOffset, nil
 }
 
-func TestBlockDeviceSize(t *testing.T) {
-	td, err := ioutil.TempDir("", "mender-block-device-")
-	assert.NoError(t, err)
-	defer os.RemoveAll(td)
+func (cr *CallRecorder) Close() error {
+	cr.Calls = append(cr.Calls, Call{
+		methodType: Method_Close,
+	})
+	return nil
+}
 
-	// prepare fake block device file
-	bdpath := path.Join(td, "foo")
-	err = createFile(bdpath)
-	assert.NoError(t, err)
+type FakeBlockDeviceFile struct {
+	*CallRecorder
+	filepath   string
+	size       uint64
+	sectorsize int
+}
 
-	// temporarily override helper for getting block device size
-	old := BlockDeviceGetSizeOf
+func (fb *FakeBlockDeviceFile) Filepath() string {
+	return fb.filepath
+}
 
-	// pretend the device is only 10 bytes in size
-	BlockDeviceGetSizeOf = makeBlockDeviceSize(t, 10, nil, bdpath)
+func (fb *FakeBlockDeviceFile) Size() (uint64, error) {
+	return fb.size, nil
+}
 
-	bd := BlockDevice{Path: bdpath}
-	sz, err := bd.Size()
-	assert.Equal(t, uint64(10), sz)
-	assert.NoError(t, err)
+func (fb *FakeBlockDeviceFile) SectorSize() (int, error) {
+	return fb.sectorsize, nil
+}
 
-	BlockDeviceGetSizeOf = makeBlockDeviceSize(t, 10, errors.New("failed"), bdpath)
+type JunkReader struct {
+	val byte
+}
 
-	bd = BlockDevice{Path: bdpath}
-	sz, err = bd.Size()
-	assert.EqualError(t, err, "failed")
+func (jr JunkReader) Read(b []byte) (int, error) {
+	for idx := range b {
+		b[idx] = jr.val
+	}
+	return len(b), nil
+}
 
-	BlockDeviceGetSizeOf = old
+func TestBlockDeviceWriter(t *testing.T) {
+	type args struct {
+		initialOffset        int64
+		imageSize            int64
+		flushSize            uint64
+		additionalImageBytes int64 // added to the total bytes returned by the Reader, making the image longer or shorter than it "should" be
+	}
+	tests := []args{
+		{
+			initialOffset: 0,
+			imageSize:     1*1024*1024 + 17,
+			flushSize:     4096,
+		},
+		{
+			initialOffset: 1000,
+			imageSize:     1*1024*1024 + 17,
+			flushSize:     4096,
+		},
+		{
+			initialOffset: 10000,
+			imageSize:     2*1024*1024 + 17,
+			flushSize:     16 * 1024,
+		},
+		{
+			initialOffset:        10000,
+			imageSize:            2*1024*1024 + 17,
+			flushSize:            16 * 1024,
+			additionalImageBytes: 10, // should trigger an error
+		},
+		{
+			initialOffset:        10000,
+			imageSize:            2*1024*1024 + 17,
+			flushSize:            16 * 1024,
+			additionalImageBytes: -10, // should trigger an error
+		},
+	}
+
+	for testIdx, tt := range tests {
+		t.Run(fmt.Sprintf("subtest%02d", testIdx), func(t *testing.T) {
+			DoTestNewBlockDeviceWriter(t, tt.initialOffset, tt.imageSize, tt.flushSize, tt.additionalImageBytes)
+		})
+	}
+}
+
+func DoTestNewBlockDeviceWriter(t *testing.T, INITIAL_OFFSET int64, IMAGE_SIZE int64, FLUSH_SIZE uint64, ADDITIONAL_IMAGE_BYTES int64) {
+
+	cr := &CallRecorder{}
+	fbd := &FakeBlockDeviceFile{
+		CallRecorder: cr,
+		filepath:     "/dev/fakep1",
+		size:         100 * 1024 * 1024,
+		sectorsize:   512,
+	}
+
+	progressCallback := func(blockStartByte, blockEndByte int64) {
+		cr.Calls = append(cr.Calls, Call{
+			methodType:             Method_ProgressCallback,
+			progressBlockStartByte: blockStartByte,
+			progressBlockEndByte:   blockEndByte,
+		})
+	}
+
+	bdw, err := NewBlockDeviceWriter(
+		fbd,
+		IMAGE_SIZE,
+		FLUSH_SIZE,
+		progressCallback,
+	)
+
+	_, err = bdw.Seek(INITIAL_OFFSET, io.SeekStart)
+	require.NoError(t, err)
+
+	reader := io.LimitReader(JunkReader{val: 42}, IMAGE_SIZE-INITIAL_OFFSET+ADDITIONAL_IMAGE_BYTES)
+	bytes_read, err := bdw.ReadFrom(reader)
+
+	require.NoError(t, err)
+
+	if ADDITIONAL_IMAGE_BYTES != 0 {
+		// we expect an error in this case
+		require.Error(t, bdw.CheckFullImageWritten())
+		require.Error(t, bdw.Close())
+	} else {
+		// we expect no error in this case
+		require.Equal(t, bytes_read, IMAGE_SIZE-INITIAL_OFFSET, "didn't read whole image")
+		require.NoError(t, bdw.CheckFullImageWritten())
+		require.NoError(t, bdw.Close())
+	}
+
+	// Check a bunch of invariants
+	totalBytesSeenOnBlockDevice := int64(0)
+	totalWrites := 0
+	totalSyncs := 0
+	totalProgressCallbacks := 0
+	bytesSinceLastSync := 0
+
+	allWriteSizes := []int{}
+	lastSyncedOffset := int64(0)
+
+	for idx, call := range cr.Calls {
+		switch call.methodType {
+		case Method_Write:
+			writeLen := len(call.writeData)
+			allWriteSizes = append(allWriteSizes, writeLen)
+			totalWrites += 1
+			totalBytesSeenOnBlockDevice += int64(writeLen)
+			bytesSinceLastSync += writeLen
+		case Method_Sync:
+			assert.True(t, uint64(bytesSinceLastSync) <= FLUSH_SIZE,
+				"should sync at least once every %d bytes, but it's been %d bytes",
+				FLUSH_SIZE, bytesSinceLastSync)
+			lastSyncedOffset = INITIAL_OFFSET + totalBytesSeenOnBlockDevice
+			bytesSinceLastSync = 0
+			totalSyncs += 1
+		case Method_Seek:
+			didAnything := !(call.seekWhence == io.SeekCurrent && call.seekOffset == 0)
+			if didAnything && idx != 0 {
+				t.Errorf("Seek() must be first call (idx 0, not %d)", idx)
+			}
+		case Method_Close:
+			if idx != len(cr.Calls)-1 {
+				t.Errorf("Close() must be last call (seen at idx %d, of %d)", idx, len(cr.Calls)-1)
+			}
+		case Method_ProgressCallback:
+			totalProgressCallbacks += 1
+			if call.progressBlockEndByte > lastSyncedOffset {
+				t.Errorf("progress callback invoked too early: blockEndBytes is %d but lastSyncedOffset is %d",
+					call.progressBlockEndByte, lastSyncedOffset,
+				)
+			}
+		}
+	}
+
+	for idx, writeSize := range allWriteSizes[1 : len(allWriteSizes)-1] {
+		assert.Equal(t, uint64(writeSize), FLUSH_SIZE, "only the first and last chunks can be incomplete, but idx %d is %d", idx+1, writeSize)
+	}
+	assert.True(t, uint64(allWriteSizes[0]) <= FLUSH_SIZE, "all write sizes must be at most FLUSH_SIZE (%d), but first was %d", FLUSH_SIZE, allWriteSizes[0])
+	lastWriteSize := uint64(allWriteSizes[len(allWriteSizes)-1])
+	assert.True(t, lastWriteSize <= FLUSH_SIZE, "all write sizes must be at most FLUSH_SIZE (%d), but last was %d", FLUSH_SIZE, lastWriteSize)
+
+	assert.True(t, totalSyncs > 0, "Sync() was never called")
+	assert.True(t, totalProgressCallbacks > 0, "progress callback was never called")
+
+	assert.Equal(t, IMAGE_SIZE-INITIAL_OFFSET+ADDITIONAL_IMAGE_BYTES, totalBytesSeenOnBlockDevice, "bytes written mismatch")
 }
