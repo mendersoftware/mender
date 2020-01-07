7963cd0af1d0ae3cbf05f7bc18b129ec07905807
Ole Petter <ole.orhagen@northern.tech>
2020-01-07 09:54:03 +0100

    Remove unused variables discovered by new varcheck
    Changelog: None
    
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- cli/cli.go
+++ cli/cli.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Northern.tech AS
+// Copyright 2020 Northern.tech AS
 //
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
@@ -57,8 +57,6 @@ const (
 	snapshotDescription = "Creates a snapshot of the currently running " +
 		"rootfs. Refer to the list of commands to specify where to " +
 		"stream the image."
-	snapshotDumpDescription = "Dump rootfs to standard out. Exits if " +
-		"output isn't redirected."
 )
 
 const (
