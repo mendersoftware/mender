b0be06dbab931f319f16d813d51e8b435313e96d
Kristian Amlie <kristian.amlie@northern.tech>
2020-03-18 15:13:29 +0100

    Add handling of TENANT_TOKEN to docker-client test container.
    Changelog: None
    
    Signed-off-by: Kristian Amlie <kristian.amlie@northern.tech>
    (cherry picked from commit 993b46363a3766f428919a25091440ca19d19490)

---
+++ tests/entrypoint.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+
+set -e
+
+if [ -n "$TENANT_TOKEN" ]; then
+    sed -i -e "s/\"TenantToken\": *\"[^\"]*\"/\"TenantToken\": \"$TENANT_TOKEN\"/" /etc/mender/mender.conf
+fi
+
+/etc/init.d/ssh start
+mender -daemon
