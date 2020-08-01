acf1da3e9daf7ad1d435aca303521db37c0e1221
Peter Grzybowski <peter@northern.tech>
2020-08-01 20:27:06 +0200

    take 6
    ChangeLog:none
    Signed-off-by: Peter Grzybowski <peter@northern.tech>

--- client/client.go
+++ client/client.go
@@ -281,29 +281,33 @@ func newHttpClient() *http.Client {
 	return &http.Client{}
 }
 
-//var globalConfig Config //just for testing
-//
-//func dialOpenSSL(network string, addr string) (net.Conn, error) {
-//	//contextSSL := ctx.Value("ssl").(*openssl.Ctx)
-//	contextSSL, err := openssl.NewCtx() // probably should consider reusing the context, but we
-//	// have to propagate it with request, to get it here see^
-//	err = contextSSL.LoadVerifyLocations(globalConfig.ServerCert, "")
-//	flags := openssl.DialFlags(0)
-//
-//	if globalConfig.NoVerify {
-//		flags = openssl.InsecureSkipHostVerification
-//	}
-//
-//	conn, err := openssl.Dial("tcp", addr, contextSSL, flags)
-//	v := conn.VerifyResult()
-//	if v != openssl.Ok && int(v) != 66 {
-//		if v == openssl.CertHasExpired {
-//			return nil, errors.New("certificate has expired")
-//		}
-//		return nil, errors.New(fmt.Sprintf("not a valid certificate, openssl says: %d", v))
-//	}
-//	return conn, err
-//}
+func dialOpenSSL(conf Config, network string, addr string) (net.Conn, error) {
+	contextSSL, err := openssl.NewCtx() // probably should consider reusing the context, but we
+	// have to propagate it with every request
+	err = contextSSL.LoadVerifyLocations(conf.ServerCert, "")
+	if err != nil {
+		return nil, err
+	}
+	flags := openssl.DialFlags(0)
+
+	if conf.NoVerify {
+		flags = openssl.InsecureSkipHostVerification
+	}
+
+	conn, err := openssl.Dial("tcp", addr, contextSSL, flags)
+	if conn == nil || err != nil {
+		return nil, err
+	}
+
+	v := conn.VerifyResult()
+	if v != openssl.Ok {
+		if v == openssl.CertHasExpired || v == openssl.DepthZeroSelfSignedCert {
+			return nil, errors.New(fmt.Sprintf("certificate has expired, openssl verify rc: %d file: %s", v, conf.ServerCert))
+		}
+		return nil, errors.New(fmt.Sprintf("not a valid certificate, openssl verify rc: %d file: %s", v, conf.ServerCert))
+	}
+	return conn, err
+}
 
 func newHttpsClient(conf Config) (*http.Client, error) {
 	client := newHttpClient()
@@ -322,32 +326,7 @@ func newHttpsClient(conf Config) (*http.Client, error) {
 		TLSClientConfig: &tlsc,
 		Proxy:           http.ProxyFromEnvironment,
 		DialTLS: func(network string, addr string) (net.Conn, error) {
-			//contextSSL := ctx.Value("ssl").(*openssl.Ctx)
-			contextSSL, err := openssl.NewCtx() // probably should consider reusing the context, but we
-			// have to propagate it with request, to get it here see^
-			err = contextSSL.LoadVerifyLocations(conf.ServerCert, "")
-			if err != nil {
-				return nil, err
-			}
-			flags := openssl.DialFlags(0)
-
-			if conf.NoVerify {
-				flags = openssl.InsecureSkipHostVerification
-			}
-
-			conn, err := openssl.Dial("tcp", addr, contextSSL, flags)
-			if conn == nil || err != nil {
-				return nil, err
-			}
-
-			v := conn.VerifyResult()
-			if v != openssl.Ok {
-				if v == openssl.CertHasExpired || v == openssl.DepthZeroSelfSignedCert {
-					return nil, errors.New(fmt.Sprintf("certificate has expired, openssl says: %d", v))
-				}
-				return nil, errors.New(fmt.Sprintf("not a valid certificate, openssl says: %d", v))
-			}
-			return conn, err
+			return dialOpenSSL(conf, network, addr)
 		},
 	}
 
