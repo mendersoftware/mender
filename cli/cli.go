7696bef98dccf9026f8cd18b6da88dbd050358de
Alf-Rune Siqveland <alf.rune@northern.tech>
2021-04-09 09:53:49 +0200

    Update copyright header
    changelog: none
    
    Signed-off-by: Alf-Rune Siqveland <alf.rune@northern.tech>

--- cli/cli.go
+++ cli/cli.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Northern.tech AS
+// Copyright 2021 Northern.tech AS
 //
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
