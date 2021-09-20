45f21bd299ef6b445458b92acb835f0f339e7354
Fabio Tranchitella <fabio.tranchitella@northern.tech>
2021-09-20 09:21:12 +0200

    QA-187: make ServerURL configurable in the Docker client
    Changelog: none
    
    Signed-off-by: Fabio Tranchitella <fabio.tranchitella@northern.tech>

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+if [ -n "$SERVER_URL" ]; then
+    sed -i -e "s/\"ServerURL\": *\"[^\"]*\"/\"ServerURL\": \"$SERVER_URL\"/" /etc/mender/mender.conf
+fi
 if [ -n "$TENANT_TOKEN" ]; then
     sed -i -e "s/\"TenantToken\": *\"[^\"]*\"/\"TenantToken\": \"$TENANT_TOKEN\"/" /etc/mender/mender.conf
 fi
