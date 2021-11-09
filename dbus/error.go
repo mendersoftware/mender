7970fadb8b378771522345d602a2a9f900798560
Lluis Campos <lluis.campos@northern.tech>
2021-11-09 13:25:11 +0100

    pipeline: Switch to golang 1.17
    This only affects unit tests, static checks, etc. The actual building of
    the binary happens either in meta-mender or mender-dist-packages, which
    might have different golang versions.
    
    Changelog: None
    
    Signed-off-by: Lluis Campos <lluis.campos@northern.tech>

--- dbus/error.go
+++ dbus/error.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Northern.tech AS
+// Copyright 2021 Northern.tech AS
 //
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
@@ -11,6 +11,7 @@
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
+//go:build !nodbus && cgo
 // +build !nodbus,cgo
 
 package dbus
