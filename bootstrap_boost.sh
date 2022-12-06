0a1ac4f12118775a14e63f8fdecd29a8b484007a
Lluis Campos <lluis.campos@northern.tech>
2022-12-06 11:24:49 +0100

    build: Vendor Boost libraries 1.80.0 as a git submodule
    Adding a bootstrap script to initialize (and build) the minimum set that
    we need for the prototype.
    
    Ticket: MEN-5795
    Changelog: None
    
    Signed-off-by: Lluis Campos <lluis.campos@northern.tech>

---
+++ bootstrap_boost.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+JSON_LIB_DEPENDS_SUBMODULES=" \
+    libs/json \
+    libs/align \
+    libs/assert \
+    libs/config \
+    libs/container \
+    libs/core \
+    libs/exception \
+    libs/headers \
+    libs/intrusive \
+    libs/move \
+    libs/mp11 \
+    libs/static_assert \
+    libs/system \
+    libs/throw_exception \
+"
+
+(
+    cd "$(dirname "$0")/vendor/boost"
+    git submodule init
+    git submodule update tools/build tools/boost_install
+    git submodule update $JSON_LIB_DEPENDS_SUBMODULES
+    ./bootstrap.sh
+    ./b2 --build-dir=build --prefix=install link=static install
+)
