db9147f127a3283f0ad243d9f43204a9ad01ce18
Ole Petter <ole.orhagen@northern.tech>
2022-12-14 15:40:29 +0100

    chore(vendor/boost): Remove the vendored Boost library
    Ticket: MEN-6123
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- autogen.sh
+++ autogen.sh
@@ -20,11 +20,6 @@ if [ ! -f "$(dirname "$0")/vendor/googletest/README.md" ] ; then
     exit 1
 fi
 
-if [ ! -f "$(dirname "$0")/vendor/boost/b2" ] ; then
-    $(dirname "$0")/bootstrap_boost.sh
-fi
-
 cd "$(dirname "$0")"
 aclocal --install
 autoreconf --install
-
