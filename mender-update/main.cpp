f1a4a4470ba89eb03ffaddc8a52237330cd7cda7
Vratislav Podzimek <v.podzimek@mykolab.com>
2022-12-19 12:48:23 +0100

    chore: Add LMDB-based key-value DB interface
    Just a simple hello-world using LMDB to prove that we can use it
    (as a dependency using pkg-config).
    
    Ticket: MEN-6121
    Changelog: None
    Signed-off-by: Vratislav Podzimek <v.podzimek@mykolab.com>

--- mender-update/main.cpp
+++ mender-update/main.cpp
@@ -18,19 +18,23 @@
 using namespace std;
 
 #include <common/json/json.hpp>
+#include <common/kv_db/kv_db.hpp>
 
-// This function only knows about the interface Json
-void hello_world(std::shared_ptr<json::Json> j) {
+// This function only knows about the interfaces to JSON and KeyValueDB
+void hello_world(std::shared_ptr<json::Json> j, std::shared_ptr<kv_db::KeyValueDB> db) {
 	j->hello_world();
+	db->hello_world();
 }
 
 #include <common/json/impl/nlohmann/nlohmann_json.hpp>
+#include <common/kv_db/impl/lmdb/lmdb_kv_db.hpp>
 
 int main() {
-	// It is here that we make an object from a concrete type, BoostJson.
+	// It is here that we make objects from concrete types
 	shared_ptr<json::NlohmannJson> j = make_shared<json::NlohmannJson>();
+	shared_ptr<kv_db::LMDB_KeyValueDB> db = make_shared<kv_db::LMDB_KeyValueDB>();
 
-	hello_world(j);
+	hello_world(j, db);
 
 	return 0;
 }
