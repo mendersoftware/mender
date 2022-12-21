3d695e82b627156aa18fbe4b8952c37c1da2df86
Fabio Tranchitella <fabio.tranchitella@northern.tech>
2022-12-21 04:55:56 +0100

    chore: update copyright headers and comments for the latest go version
    Changelog: None
    Ticket: None
    
    Signed-off-by: Fabio Tranchitella <fabio.tranchitella@northern.tech>

--- app/state.go
+++ app/state.go
@@ -1,16 +1,16 @@
 // Copyright 2022 Northern.tech AS
 //
-//    Licensed under the Apache License, Version 2.0 (the "License");
-//    you may not use this file except in compliance with the License.
-//    You may obtain a copy of the License at
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
 //
-//        http://www.apache.org/licenses/LICENSE-2.0
+//	    http://www.apache.org/licenses/LICENSE-2.0
 //
-//    Unless required by applicable law or agreed to in writing, software
-//    distributed under the License is distributed on an "AS IS" BASIS,
-//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//    See the License for the specific language governing permissions and
-//    limitations under the License.
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
 package app
 
 import (
@@ -2206,7 +2206,6 @@ func NewControlMapPauseState(wrappedState UpdateState) State {
 //
 // 1. a timeout, which means we need to refresh the map
 // 2. an update map expires, which means we need to query the next action
-//
 func (c *controlMapPauseState) Handle(ctx *StateContext, controller Controller) (State, bool) {
 
 	log.Debug("Handle controlMapPause state")
