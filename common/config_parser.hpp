da672d16e5fa5609d72d6d4f856159cf4bea7657
Vratislav Podzimek <v.podzimek@mykolab.com>
2023-01-12 15:12:23 +0100

    feat: The MenderConfigFromFile class
    Ticket: MEN-6112
    Changelog: None
    
    Signed-off-by: Vratislav Podzimek <v.podzimek@mykolab.com>

---
+++ common/config_parser.hpp
@@ -0,0 +1,161 @@
+// Copyright 2023 Northern.tech AS
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+#include <string>
+#include <vector>
+#include <common/error.hpp>
+#include <common/expected.hpp>
+
+#ifndef MENDER_COMMON_CONFIG_PARSER_HPP
+#define MENDER_COMMON_CONFIG_PARSER_HPP
+
+namespace mender::common::config_parser {
+
+using namespace std;
+
+/** HttpsClient holds the configuration for the client side mTLS
+	configuration */
+struct HttpsClient {
+	string certificate;
+	string key;
+	string ssl_engine;
+};
+
+/** Security structure holds the configuration for the client Added for MEN-3924
+	in order to provide a way to specify PKI params outside HttpsClient. */
+struct ClientSecurity {
+	string auth_private_key;
+	string ssl_engine;
+};
+
+/** Connectivity instructs the client how we want to treat the keep alive
+	connections and when a connection is considered idle and therefore closed */
+struct ClientConnectivity {
+	bool disable_keep_alive = false;
+	int idle_conn_timeout_seconds = 0;
+};
+
+enum ConfigParserErrorCode {
+	ParseError,
+};
+
+using ConfigParserError = mender::common::error::Error<ConfigParserErrorCode>;
+using ExpectedBool = mender::common::expected::Expected<bool, ConfigParserError>;
+
+class MenderConfigFromFile {
+public:
+	/** Path to the public key used to verify signed updates.
+		Only one of artifact_verify_key/artifact_verify_keys can be specified. */
+	string artifact_verify_key;
+
+	/** List of verification keys for verifying signed updates.
+		Starting in order from the first key in the list,
+		each key will try to verify the artifact until one succeeds.
+		Only one of artifact_verify_key/artifact_verify_keys can be specified. */
+	vector<string> artifact_verify_keys;
+
+	/** HTTPS client parameters */
+	HttpsClient https_client;
+
+	/** Security parameters */
+	ClientSecurity security;
+
+	/** Connectivity connection handling and transfer parameters */
+	ClientConnectivity connectivity;
+
+	/** Rootfs device paths */
+	string rootfs_part_A;
+	string rootfs_part_B;
+
+	/** Command to set active partition */
+	string boot_utilities_set_active_part;
+
+	/** Command to get the partition which will boot next */
+	string boot_utilities_get_next_active_part;
+
+	/** Path to the device type file */
+	string device_type_file;
+
+	/** DBus configuration */
+	// TODO: struct DbusConfig { bool enabled; }; ?
+	bool dbus_enabled = false;
+	;
+
+	/** Expiration timeout for the control map */
+	int update_control_map_expiration_time_seconds = 0;
+
+	/** Expiration timeout for the control map when just booted */
+	int update_control_map_boot_expiration_time_seconds = 0;
+
+	/** Poll interval for checking for new updates */
+	int update_poll_interval_seconds = 0;
+
+	/** Poll interval for periodically sending inventory data */
+	int inventory_poll_interval_seconds = 0;
+
+	/** Skip CA certificate validation */
+	bool skip_verify = false;
+
+	/** Global retry polling max interval for fetching update, authorize wait and update status */
+	int retry_poll_interval_seconds = 0;
+
+	/** Global max retry poll count */
+	int retry_poll_count = 0;
+
+	/* State script parameters */
+	int state_script_timeout_seconds = 0;
+	int state_script_retry_timeout_seconds = 0;
+
+	/** Poll interval for checking for update (check-update) */
+	int state_script_retry_interval_seconds = 0;
+
+	/* Update module parameters */
+	/** The timeout for the execution of the update module, after which it will
+		be killed. */
+	int module_timeout_seconds = 0;
+
+	/** Path to server SSL certificate */
+	string server_certificate;
+
+	/** Server URL (For single server conf) */
+	string server_url;
+
+	/** Path to deployment log file */
+	string update_log_path;
+
+	/** Server JWT TenantToken */
+	string tenant_token;
+
+	/** List of available servers, to which client can fall over */
+	// TODO: struct MenderServer {string url;}; ?
+	vector<string> servers;
+
+	/** Log level which takes effect right before daemon startup */
+	string daemon_log_level;
+
+	/**
+	 * Loads values from the given file and overrides the current values of the
+	 * respective above fields with them.
+	 *
+	 * @return whether some new values were actually applied or not
+	 * @note Invalid JSON data is ignored -- the whole file if it's not a valid
+	 *       JSON file and if it is then extra fields are ignored and fields of
+	 *       unexpected types are ignored too.
+	 */
+	ExpectedBool LoadFile(const string &path);
+};
+
+} // namespace mender::common::config_parser
+
+#endif // MENDER_COMMON_CONFIG_PARSER_HPP
---
+++ common/config_parser/config_parser.cpp
@@ -0,0 +1,412 @@
+// Copyright 2023 Northern.tech AS
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+#include <common/config_parser.hpp>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <common/json.hpp>
+
+namespace mender::common::config_parser {
+
+using namespace std;
+namespace json = mender::common::json;
+
+ExpectedBool MenderConfigFromFile::LoadFile(const string &path) {
+	const json::ExpectedJson e_cfg_json = json::LoadFromFile(path);
+	if (!e_cfg_json) {
+		ConfigParserError err = {
+			ConfigParserErrorCode::ParseError,
+			"Failed to parse '" + path + "': " + e_cfg_json.error().message};
+		return ExpectedBool(err);
+	}
+
+	bool applied = false;
+
+	/* Deal with plain string values first */
+	const json::Json cfg_json = e_cfg_json.value();
+	json::ExpectedJson e_cfg_value = cfg_json.Get("ArtifactVerifyKey");
+	if (e_cfg_value) {
+		const json::Json value_json = e_cfg_value.value();
+		const json::ExpectedString e_cfg_string = value_json.GetString();
+		if (e_cfg_string) {
+			this->artifact_verify_key = e_cfg_string.value();
+			applied = true;
+		}
+	}
+
+	e_cfg_value = cfg_json.Get("RootfsPartA");
+	if (e_cfg_value) {
+		const json::Json value_json = e_cfg_value.value();
+		const json::ExpectedString e_cfg_string = value_json.GetString();
+		if (e_cfg_string) {
+			this->rootfs_part_A = e_cfg_string.value();
+			applied = true;
+		}
+	}
+
+	e_cfg_value = cfg_json.Get("RootfsPartB");
+	if (e_cfg_value) {
+		const json::Json value_json = e_cfg_value.value();
+		const json::ExpectedString e_cfg_string = value_json.GetString();
+		if (e_cfg_string) {
+			this->rootfs_part_B = e_cfg_string.value();
+			applied = true;
+		}
+	}
+
+	e_cfg_value = cfg_json.Get("BootUtilitiesSetActivePart");
+	if (e_cfg_value) {
+		const json::Json value_json = e_cfg_value.value();
+		const json::ExpectedString e_cfg_string = value_json.GetString();
+		if (e_cfg_string) {
+			this->boot_utilities_set_active_part = e_cfg_string.value();
+			applied = true;
+		}
+	}
+
+	e_cfg_value = cfg_json.Get("BootUtilitiesGetNextActivePart");
+	if (e_cfg_value) {
+		const json::Json value_json = e_cfg_value.value();
+		const json::ExpectedString e_cfg_string = value_json.GetString();
+		if (e_cfg_string) {
+			this->boot_utilities_get_next_active_part = e_cfg_string.value();
+			applied = true;
+		}
+	}
+
+	e_cfg_value = cfg_json.Get("DeviceTypeFile");
+	if (e_cfg_value) {
+		const json::Json value_json = e_cfg_value.value();
+		const json::ExpectedString e_cfg_string = value_json.GetString();
+		if (e_cfg_string) {
+			this->device_type_file = e_cfg_string.value();
+			applied = true;
+		}
+	}
+
+	e_cfg_value = cfg_json.Get("ServerCertificate");
+	if (e_cfg_value) {
+		const json::Json value_json = e_cfg_value.value();
+		const json::ExpectedString e_cfg_string = value_json.GetString();
+		if (e_cfg_string) {
+			this->server_certificate = e_cfg_string.value();
+			applied = true;
+		}
+	}
+
+	e_cfg_value = cfg_json.Get("ServerURL");
+	if (e_cfg_value) {
+		const json::Json value_json = e_cfg_value.value();
+		const json::ExpectedString e_cfg_string = value_json.GetString();
+		if (e_cfg_string) {
+			this->server_url = e_cfg_string.value();
+			applied = true;
+		}
+	}
+
+	e_cfg_value = cfg_json.Get("UpdateLogPath");
+	if (e_cfg_value) {
+		const json::Json value_json = e_cfg_value.value();
+		const json::ExpectedString e_cfg_string = value_json.GetString();
+		if (e_cfg_string) {
+			this->update_log_path = e_cfg_string.value();
+			applied = true;
+		}
+	}
+
+	e_cfg_value = cfg_json.Get("TenantToken");
+	if (e_cfg_value) {
+		const json::Json value_json = e_cfg_value.value();
+		const json::ExpectedString e_cfg_string = value_json.GetString();
+		if (e_cfg_string) {
+			this->tenant_token = e_cfg_string.value();
+			applied = true;
+		}
+	}
+
+	e_cfg_value = cfg_json.Get("DaemonLogLevel");
+	if (e_cfg_value) {
+		const json::Json value_json = e_cfg_value.value();
+		const json::ExpectedString e_cfg_string = value_json.GetString();
+		if (e_cfg_string) {
+			this->daemon_log_level = e_cfg_string.value();
+			applied = true;
+		}
+	}
+
+	/* Boolean values now */
+	e_cfg_value = cfg_json.Get("SkipVerify");
+	if (e_cfg_value) {
+		const json::Json value_json = e_cfg_value.value();
+		const json::ExpectedBool e_cfg_bool = value_json.GetBool();
+		if (e_cfg_bool) {
+			this->skip_verify = e_cfg_bool.value();
+			applied = true;
+		}
+	}
+
+	e_cfg_value = cfg_json.Get("DBus");
+	if (e_cfg_value) {
+		const json::Json value_json = e_cfg_value.value();
+		const json::ExpectedJson e_cfg_subval = value_json.Get("Enabled");
+		if (e_cfg_subval) {
+			const json::Json subval_json = e_cfg_subval.value();
+			const json::ExpectedBool e_cfg_bool = subval_json.GetBool();
+			if (e_cfg_bool) {
+				this->dbus_enabled = e_cfg_bool.value();
+				applied = true;
+			}
+		}
+	}
+
+	/* Integer values */
+	e_cfg_value = cfg_json.Get("UpdateControlMapExpirationTimeSeconds");
+	if (e_cfg_value) {
+		const json::Json value_json = e_cfg_value.value();
+		const json::ExpectedInt e_cfg_int = value_json.GetInt();
+		if (e_cfg_int) {
+			this->update_control_map_expiration_time_seconds = e_cfg_int.value();
+			applied = true;
+		}
+	}
+
+	e_cfg_value = cfg_json.Get("UpdateControlMapBootExpirationTimeSeconds");
+	if (e_cfg_value) {
+		const json::Json value_json = e_cfg_value.value();
+		const json::ExpectedInt e_cfg_int = value_json.GetInt();
+		if (e_cfg_int) {
+			this->update_control_map_boot_expiration_time_seconds = e_cfg_int.value();
+			applied = true;
+		}
+	}
+
+	e_cfg_value = cfg_json.Get("UpdatePollIntervalSeconds");
+	if (e_cfg_value) {
+		const json::Json value_json = e_cfg_value.value();
+		const json::ExpectedInt e_cfg_int = value_json.GetInt();
+		if (e_cfg_int) {
+			this->update_poll_interval_seconds = e_cfg_int.value();
+			applied = true;
+		}
+	}
+
+	e_cfg_value = cfg_json.Get("InventoryPollIntervalSeconds");
+	if (e_cfg_value) {
+		const json::Json value_json = e_cfg_value.value();
+		const json::ExpectedInt e_cfg_int = value_json.GetInt();
+		if (e_cfg_int) {
+			this->inventory_poll_interval_seconds = e_cfg_int.value();
+			applied = true;
+		}
+	}
+
+	e_cfg_value = cfg_json.Get("RetryPollIntervalSeconds");
+	if (e_cfg_value) {
+		const json::Json value_json = e_cfg_value.value();
+		const json::ExpectedInt e_cfg_int = value_json.GetInt();
+		if (e_cfg_int) {
+			this->retry_poll_interval_seconds = e_cfg_int.value();
+			applied = true;
+		}
+	}
+
+	e_cfg_value = cfg_json.Get("RetryPollCount");
+	if (e_cfg_value) {
+		const json::Json value_json = e_cfg_value.value();
+		const json::ExpectedInt e_cfg_int = value_json.GetInt();
+		if (e_cfg_int) {
+			this->retry_poll_count = e_cfg_int.value();
+			applied = true;
+		}
+	}
+
+	e_cfg_value = cfg_json.Get("StateScriptTimeoutSeconds");
+	if (e_cfg_value) {
+		const json::Json value_json = e_cfg_value.value();
+		const json::ExpectedInt e_cfg_int = value_json.GetInt();
+		if (e_cfg_int) {
+			this->state_script_timeout_seconds = e_cfg_int.value();
+			applied = true;
+		}
+	}
+
+	e_cfg_value = cfg_json.Get("StateScriptRetryTimeoutSeconds");
+	if (e_cfg_value) {
+		const json::Json value_json = e_cfg_value.value();
+		const json::ExpectedInt e_cfg_int = value_json.GetInt();
+		if (e_cfg_int) {
+			this->state_script_retry_timeout_seconds = e_cfg_int.value();
+			applied = true;
+		}
+	}
+
+	e_cfg_value = cfg_json.Get("StateScriptRetryIntervalSeconds");
+	if (e_cfg_value) {
+		const json::Json value_json = e_cfg_value.value();
+		const json::ExpectedInt e_cfg_int = value_json.GetInt();
+		if (e_cfg_int) {
+			this->state_script_retry_interval_seconds = e_cfg_int.value();
+			applied = true;
+		}
+	}
+
+	e_cfg_value = cfg_json.Get("ModuleTimeoutSeconds");
+	if (e_cfg_value) {
+		const json::Json value_json = e_cfg_value.value();
+		const json::ExpectedInt e_cfg_int = value_json.GetInt();
+		if (e_cfg_int) {
+			this->module_timeout_seconds = e_cfg_int.value();
+			applied = true;
+		}
+	}
+
+	/* Vectors/arrays now */
+	e_cfg_value = cfg_json.Get("ArtifactVerifyKeys");
+	if (e_cfg_value) {
+		const json::Json value_array = e_cfg_value.value();
+		const json::ExpectedSize e_n_items = value_array.GetArraySize();
+		if (e_n_items) {
+			for (size_t i = 0; i < e_n_items.value(); i++) {
+				const json::ExpectedJson e_array_item = value_array.Get(i);
+				if (e_array_item) {
+					const json::ExpectedString e_item_string = e_array_item.value().GetString();
+					if (e_item_string) {
+						const string item_value = e_item_string.value();
+						if (count(
+								this->artifact_verify_keys.begin(),
+								this->artifact_verify_keys.end(),
+								item_value)
+							== 0) {
+							this->artifact_verify_keys.push_back(item_value);
+							applied = true;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	e_cfg_value = cfg_json.Get("Servers");
+	if (e_cfg_value) {
+		const json::Json value_array = e_cfg_value.value();
+		const json::ExpectedSize e_n_items = value_array.GetArraySize();
+		if (e_n_items) {
+			for (size_t i = 0; i < e_n_items.value(); i++) {
+				const json::ExpectedJson e_array_item = value_array.Get(i);
+				if (e_array_item) {
+					const json::ExpectedJson e_item_json = e_array_item.value().Get("ServerURL");
+					if (e_item_json) {
+						const json::ExpectedString e_item_string = e_item_json.value().GetString();
+						if (e_item_string) {
+							const string item_value = e_item_string.value();
+							if (count(this->servers.begin(), this->servers.end(), item_value)
+								== 0) {
+								this->servers.push_back(item_value);
+								applied = true;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	/* Last but not least, complex values */
+	e_cfg_value = cfg_json.Get("HttpsClient");
+	if (e_cfg_value) {
+		const json::Json value_json = e_cfg_value.value();
+		json::ExpectedJson e_cfg_subval = value_json.Get("Certificate");
+		if (e_cfg_subval) {
+			const json::Json subval_json = e_cfg_subval.value();
+			const json::ExpectedString e_cfg_string = subval_json.GetString();
+			if (e_cfg_string) {
+				this->https_client.certificate = e_cfg_string.value();
+				applied = true;
+			}
+		}
+
+		e_cfg_subval = value_json.Get("Key");
+		if (e_cfg_subval) {
+			const json::Json subval_json = e_cfg_subval.value();
+			const json::ExpectedString e_cfg_string = subval_json.GetString();
+			if (e_cfg_string) {
+				this->https_client.key = e_cfg_string.value();
+				applied = true;
+			}
+		}
+
+		e_cfg_subval = value_json.Get("SSLEngine");
+		if (e_cfg_subval) {
+			const json::Json subval_json = e_cfg_subval.value();
+			const json::ExpectedString e_cfg_string = subval_json.GetString();
+			if (e_cfg_string) {
+				this->https_client.ssl_engine = e_cfg_string.value();
+				applied = true;
+			}
+		}
+	}
+
+	e_cfg_value = cfg_json.Get("Security");
+	if (e_cfg_value) {
+		const json::Json value_json = e_cfg_value.value();
+		json::ExpectedJson e_cfg_subval = value_json.Get("AuthPrivateKey");
+		if (e_cfg_subval) {
+			const json::Json subval_json = e_cfg_subval.value();
+			const json::ExpectedString e_cfg_string = subval_json.GetString();
+			if (e_cfg_string) {
+				this->security.auth_private_key = e_cfg_string.value();
+				applied = true;
+			}
+		}
+
+		e_cfg_subval = value_json.Get("SSLEngine");
+		if (e_cfg_subval) {
+			const json::Json subval_json = e_cfg_subval.value();
+			const json::ExpectedString e_cfg_string = subval_json.GetString();
+			if (e_cfg_string) {
+				this->security.ssl_engine = e_cfg_string.value();
+				applied = true;
+			}
+		}
+	}
+
+	e_cfg_value = cfg_json.Get("Connectivity");
+	if (e_cfg_value) {
+		const json::Json value_json = e_cfg_value.value();
+		json::ExpectedJson e_cfg_subval = value_json.Get("DisableKeepAlive");
+		if (e_cfg_subval) {
+			const json::Json subval_json = e_cfg_subval.value();
+			const json::ExpectedBool e_cfg_bool = subval_json.GetBool();
+			if (e_cfg_bool) {
+				this->connectivity.disable_keep_alive = e_cfg_bool.value();
+				applied = true;
+			}
+		}
+
+		e_cfg_subval = value_json.Get("IdleConnTimeoutSeconds");
+		if (e_cfg_subval) {
+			const json::Json subval_json = e_cfg_subval.value();
+			const json::ExpectedInt e_cfg_int = subval_json.GetInt();
+			if (e_cfg_int) {
+				this->connectivity.idle_conn_timeout_seconds = e_cfg_int.value();
+				applied = true;
+			}
+		}
+	}
+
+	return ExpectedBool(applied);
+}
+
+} // namespace mender::common::config_parser
