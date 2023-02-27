d7af67ba682571d9ebe6762dc605e4c31c199dc6
Vratislav Podzimek <v.podzimek@mykolab.com>
2023-02-27 16:31:26 +0100

    feat: Add CmdlineOptionsIterator
    A rudimentary implementation of a command-line options+arguments parser.
    
    Ticket: MEN-6309
    Changelog: none
    Signed-off-by: Vratislav Podzimek <v.podzimek@mykolab.com>

--- common/conf.hpp
+++ common/conf.hpp
@@ -16,14 +16,60 @@
 #define MENDER_COMMON_CONF_HPP
 
 #include <string>
+#include <vector>
+#include <unordered_set>
 
 namespace mender {
 namespace common {
 namespace conf {
 
 using namespace std;
+namespace error = mender::common::error;
+namespace expected = mender::common::expected;
+
+enum ConfigErrorCode {
+	NoError = 0,
+	InvalidOptionsError,
+};
+
+class ConfigErrorCategoryClass : public std::error_category {
+public:
+	const char *name() const noexcept override;
+	string message(int code) const override;
+};
+extern const ConfigErrorCategoryClass ConfigErrorCategory;
+
+error::Error MakeError(ConfigErrorCode code, const string &msg);
+
 
 string GetEnv(const string &var_name, const string &default_value);
+
+struct OptionValue {
+	string option;
+	string value;
+};
+
+using OptsSet = unordered_set<string>;
+using ExpectedOptionValue = expected::expected<OptionValue, error::Error>;
+
+class CmdlineOptionsIterator {
+public:
+	CmdlineOptionsIterator(
+		const vector<string> &args,
+		const OptsSet &opts_with_value,
+		const OptsSet &opts_without_value) :
+		args_(args),
+		opts_with_value_(opts_with_value),
+		opts_wo_value_(opts_without_value) {};
+	ExpectedOptionValue Next();
+
+private:
+	const vector<string> &args_;
+	OptsSet opts_with_value_;
+	OptsSet opts_wo_value_;
+	size_t pos_ = 0;
+	bool past_double_dash_ = false;
+};
 
 } // namespace conf
 } // namespace common
--- common/conf/conf.cpp
+++ common/conf/conf.cpp
@@ -17,11 +17,38 @@
 #include <string>
 #include <cstdlib>
 
+#include <common/error.hpp>
+#include <common/expected.hpp>
+
 namespace mender {
 namespace common {
 namespace conf {
 
 using namespace std;
+namespace error = mender::common::error;
+namespace expected = mender::common::expected;
+
+const ConfigErrorCategoryClass ConfigErrorCategory;
+
+const char *ConfigErrorCategoryClass::name() const noexcept {
+	return "ConfigErrorCategory";
+}
+
+string ConfigErrorCategoryClass::message(int code) const {
+	switch (code) {
+	case NoError:
+		return "Success";
+	case InvalidOptionsError:
+		return "Invalid options given";
+	default:
+		return "Unknown";
+	}
+}
+
+error::Error MakeError(ConfigErrorCode code, const string &msg) {
+	return error::Error(error_condition(code, ConfigErrorCategory), msg);
+}
+
 
 string GetEnv(const string &var_name, const string &default_value) {
 	const char *value = getenv(var_name.c_str());
@@ -30,6 +57,71 @@ string GetEnv(const string &var_name, const string &default_value) {
 	} else {
 		return string(value);
 	}
+}
+
+ExpectedOptionValue CmdlineOptionsIterator::Next() {
+	string option = "";
+	string value = "";
+
+	if (pos_ >= args_.size()) {
+		return ExpectedOptionValue({"", ""});
+	}
+
+	if (past_double_dash_) {
+		OptionValue opt_val {"", args_[pos_]};
+		pos_++;
+		return ExpectedOptionValue(opt_val);
+	}
+
+	if (args_[pos_] == "--") {
+		past_double_dash_ = true;
+		pos_++;
+		return ExpectedOptionValue({"--", ""});
+	}
+
+	if (args_[pos_][0] == '-') {
+		auto eq_idx = args_[pos_].find('=');
+		if (eq_idx != string::npos) {
+			option = args_[pos_].substr(0, eq_idx);
+			value = args_[pos_].substr(eq_idx + 1, args_[pos_].size() - eq_idx - 1);
+			pos_++;
+		} else {
+			option = args_[pos_];
+			pos_++;
+		}
+
+		if (opts_with_value_.count(option) != 0) {
+			// option with value
+			if ((value == "") && ((pos_ >= args_.size()) || (args_[pos_][0] == '-'))) {
+				// the next item is not a value
+				error::Error err = MakeError(
+					ConfigErrorCode::InvalidOptionsError, "Option " + option + " missing value");
+				return ExpectedOptionValue(expected::unexpected(err));
+			} else if (value == "") {
+				// only assign the next item as value if there was no value
+				// specified as '--opt=value' (parsed above)
+				value = args_[pos_];
+				pos_++;
+			}
+		} else if (opts_wo_value_.count(option) == 0) {
+			// unknown option
+			error::Error err = MakeError(
+				ConfigErrorCode::InvalidOptionsError, "Unrecognized option '" + option + "'");
+			return ExpectedOptionValue(expected::unexpected(err));
+		} else if (value != "") {
+			// option without a value, yet, there was a value specified as '--opt=value' (parsed
+			// above)
+			error::Error err = MakeError(
+				ConfigErrorCode::InvalidOptionsError,
+				"Option " + option + " doesn't expect a value");
+			return ExpectedOptionValue(expected::unexpected(err));
+		}
+	} else {
+		value = args_[pos_];
+		pos_++;
+	}
+
+	return ExpectedOptionValue({std::move(option), std::move(value)});
 }
 
 } // namespace conf
