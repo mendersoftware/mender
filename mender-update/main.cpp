68b35978849f5c233fb78de667733a5c6b999d12
Vratislav Podzimek <v.podzimek@mykolab.com>
2023-03-17 14:48:07 +0100

    chore: Do basic configuration in mender-update
    The same as in mender-auth.
    
    Ticket: MEN-6309
    Changelog: none
    Signed-off-by: Vratislav Podzimek <v.podzimek@mykolab.com>

--- mender-update/main.cpp
+++ mender-update/main.cpp
@@ -12,119 +12,38 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 
-#include <cstddef>
 #include <iostream>
-#include <memory>
 #include <string>
+#include <vector>
+
+#include <common/conf.hpp>
+#include <common/context.hpp>
 
 using namespace std;
 
-#include <common/error.hpp>
-#include <common/expected.hpp>
-#include <common/http.hpp>
-#include <common/json.hpp>
-#include <common/log.hpp>
-#include <common/conf/paths.hpp>
-
-using namespace mender::common;
-
-
-
-void log_poc() {
-	namespace log = mender::common::log;
-
-	auto logger = log::Logger("NamedLogger");
-
-	logger.Log(log::LogLevel::Info, "Test log");
-
-	auto sub_logger = logger.WithFields(log::LogField("foo", "bar"), log::LogField("bar", "baz"));
-
-	sub_logger.Log(log::LogLevel::Error, "Some error");
-
-	logger.Info("Some info message");
-
-	logger.SetLevel(log::LogLevel::Warning);
-
-	logger.Info("I should never show up");
-
-	logger.SetLevel(log::LogLevel::Debug);
-
-	logger.Log(log::LogLevel::Trace, "I should not show");
-	logger.Log(log::LogLevel::Debug, "I should show test");
-
-	// Global logger
-
-	log::SetLevel(log::LogLevel::Info);
-
-	log::WithFields(log::LogField("test", "ing")).Info("Bugs bunny");
-	log::Info("Foobar");
-	log::Warning("Hur-dur");
-}
-
-int main() {
-	log_poc();
-
-	namespace ExampleErrorType = mender::common::json;
-	using ExpectedExampleString = expected::expected<string, error::Error>;
-
-	ExpectedExampleString ex_s = ExpectedExampleString("Hello, world!");
-
-	auto err = ExampleErrorType::MakeError(json::KeyError, "Something wrong happened");
-	ExpectedExampleString ex_s_err = expected::unexpected(err);
-
-	if (ex_s) {
-		std::cout << "Got expected string value: '" << ex_s.value() << "'" << std::endl;
+int main(int argc, char *argv[]) {
+	mender::common::conf::MenderConfig config;
+	if (argc > 1) {
+		vector<string> args(argv + 1, argv + argc);
+		auto err = config.ProcessCmdlineArgs(args);
+		if (err) {
+			cerr << "Failed to process command line options: " + err.message << endl;
+			return 1;
+		}
 	} else {
-		std::cout << "Got (un)expected error: '" << ex_s.error().message << "'" << std::endl;
+		auto err = config.LoadDefaults();
+		if (err) {
+			cerr << "Failed to process command line options: " + err.message << endl;
+			return 1;
+		}
 	}
 
-	if (ex_s_err) {
-		std::cout << "Got expected string value: '" << ex_s_err.value() << "'" << std::endl;
-	} else {
-		std::cout << "Got (un)expected error: '" << ex_s_err.error().message << "'" << std::endl;
-	}
-
-	class PrintBody : public io::Writer {
-	public:
-		expected::ExpectedSize Write(
-			vector<uint8_t>::const_iterator start, vector<uint8_t>::const_iterator end) {
-			cout << string {start, end} << flush;
-			return end - start;
-		}
-		~PrintBody() {
-			cout << "Done!\n";
-		}
-	};
-	auto header_handler = [](mender::http::ExpectedResponsePtr result) {
-		if (!result) {
-			log::Error(result.error().String());
-			return;
-		}
-		result.value()->SetBodyWriter(make_shared<PrintBody>());
-	};
-	auto body_handler = [](mender::http::ExpectedResponsePtr result) {
-		if (!result) {
-			log::Error(result.error().String());
-		}
-	};
-
-	mender::common::events::EventLoop loop;
-	mender::http::Client http_client;
-	mender::http::Session http(http_client, loop);
-	auto req(make_shared<mender::http::Request>(mender::http::Method::GET));
-	// If you want to get a successful response, launch `python3 -m http.server` first.
-	err = req->SetAddress("http://localhost:8000/");
+	mender::common::context::MenderContext main_context;
+	auto err = main_context.Initialize(config);
 	if (err) {
-		log::Error(err.String());
-	}
-	err = http.AsyncCall(req, header_handler, body_handler);
-	if (err) {
-		log::Error(err.String());
+		cerr << "Failed to intialize main context: " + err.message << endl;
+		return 1;
 	}
 
-	loop.Run();
-
-	std::cout << "Default conf dir is: " << mender::common::conf::paths::DefaultConfFile
-			  << std::endl;
 	return 0;
 }
