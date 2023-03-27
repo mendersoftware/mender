c3ff3ec3e49a24538be75d8177e40350f7a5c057
Vratislav Podzimek <v.podzimek@mykolab.com>
2023-03-27 16:16:44 +0200

    feat: UpdateModule File API tree
    Basics of UpdateModule::PrepareFileTree() and a trivial
    implementation of UpdateModule::DeleteFileTree().
    
    Ticket: MEN-6315
    Changelog: None
    
    Signed-off-by: Vratislav Podzimek <v.podzimek@mykolab.com>
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- mender-update/update_module/v3/update_module_test.cpp
+++ mender-update/update_module/v3/update_module_test.cpp
@@ -17,16 +17,32 @@
 #include <sys/stat.h>
 
 #include <algorithm>
-#include <fstream>
 
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-#include <common/conf.hpp>
+#include <common/common.hpp>
+#include <common/path.hpp>
+#include <common/key_value_database_lmdb.hpp>
 #include <common/testing.hpp>
+#include <common/processes.hpp>
 
-namespace update_module = mender::update::update_module::v3;
+#include <mender-update/context.hpp>
+#include <string>
+#include <sstream>
+
+namespace io = mender::common::io;
+namespace error = mender::common::error;
+namespace common = mender::common;
 namespace conf = mender::common::conf;
+namespace context = mender::update::context;
+namespace update_module = mender::update::update_module::v3;
+namespace path = mender::common::path;
+namespace json = mender::common::json;
+
+
+namespace processes = mender::common::processes;
+
 
 using namespace std;
 using namespace mender::common::testing;
@@ -37,11 +53,11 @@ protected:
 	string test_scripts_dir;
 
 	bool PrepareTestScriptsDir() {
-		test_scripts_dir = temp_dir.Path() + "/modules";
+		test_scripts_dir = path::Join(temp_dir.Path(), "modules");
 		if (mkdir(test_scripts_dir.c_str(), 0700) != 0) {
 			return false;
 		}
-		test_scripts_dir += "/v3";
+		test_scripts_dir = path::Join(test_scripts_dir, "v3");
 		if (mkdir(test_scripts_dir.c_str(), 0700) != 0) {
 			return false;
 		}
@@ -50,7 +66,7 @@ protected:
 	}
 
 	bool PrepareTestFile(const string &name, bool executable) {
-		string test_file_path = test_scripts_dir + "/" + name;
+		string test_file_path {path::Join(test_scripts_dir, name)};
 		ofstream os(test_file_path);
 		os.close();
 
@@ -85,8 +101,8 @@ TEST_F(UpdateModuleTests, DiscoverUpdateModulesTest) {
 	ASSERT_TRUE(ex_modules);
 	auto modules = ex_modules.value();
 	EXPECT_EQ(modules.size(), 2);
-	EXPECT_EQ(count(modules.cbegin(), modules.cend(), test_scripts_dir + "/script1"), 1);
-	EXPECT_EQ(count(modules.cbegin(), modules.cend(), test_scripts_dir + "/script2"), 1);
+	EXPECT_EQ(count(modules.cbegin(), modules.cend(), path::Join(test_scripts_dir, "script1")), 1);
+	EXPECT_EQ(count(modules.cbegin(), modules.cend(), path::Join(test_scripts_dir, "script2")), 1);
 }
 
 TEST_F(UpdateModuleTests, DiscoverUpdateModulesNoExistTest) {
@@ -131,4 +147,189 @@ TEST_F(UpdateModuleTests, DiscoverUpdateModulesNoExecutablesTest) {
 	ASSERT_TRUE(ex_modules);
 	auto modules = ex_modules.value();
 	EXPECT_EQ(modules.size(), 0);
+}
+
+testing::AssertionResult FileContains(const string &filename, const string &expected_content) {
+	ifstream is {filename};
+	ostringstream contents_s;
+	contents_s << is.rdbuf();
+	string contents {contents_s.str()};
+	if (contents == expected_content) {
+		return testing::AssertionSuccess();
+	}
+	return testing::AssertionFailure()
+		   << "Expected: '" << expected_content << "' Got: '" << contents << "'";
+};
+
+
+testing::AssertionResult FileJsonEquals(const string &filename, const string &expected_content) {
+	ifstream is {filename};
+	json::Json contents = json::Load(is).value();
+	json::Json expected_contents = json::Load(expected_content).value();
+	if (contents.Dump() == expected_contents.Dump()) {
+		return testing::AssertionSuccess();
+	}
+	return testing::AssertionFailure()
+		   << "Expected: '" << contents.Dump() << "' Got: '" << expected_contents.Dump() << "'";
+};
+
+
+class UpdateModuleFileTreeTests : public testing::Test {
+public:
+	void SetUp() override {
+		this->cfg.data_store_dir = test_state_dir.Path();
+
+		this->ctx = make_shared<context::MenderContext>(cfg);
+		auto err = ctx->Initialize();
+		ASSERT_EQ(err, error::NoError);
+
+		auto &db = ctx->GetMenderStoreDB();
+		err = db.Write(
+			"artifact-name", common::ByteVectorFromString("artifact-name existing-artifact-name"));
+		ASSERT_EQ(err, error::NoError);
+		err = db.Write(
+			"artifact-group",
+			common::ByteVectorFromString("artifact-group existing-artifact-group"));
+		ASSERT_EQ(err, error::NoError);
+
+		ofstream os(path::Join(cfg.data_store_dir, "device_type"));
+		ASSERT_TRUE(os);
+		os << "device_type=Some device type" << endl;
+		os.close();
+
+		ASSERT_TRUE(CreateArtifact());
+		std::fstream fs {path::Join(temp_dir.Path(), "artifact.mender")};
+		io::StreamReader sr {fs};
+		auto expected_artifact = mender::artifact::parser::Parse(sr);
+		ASSERT_TRUE(expected_artifact);
+		auto artifact = expected_artifact.value();
+
+		auto expected_payload_header = mender::artifact::View(artifact, 0);
+		ASSERT_TRUE(expected_payload_header) << expected_payload_header.error().message;
+
+		this->update_payload_header = mender::artifact::View(artifact, 0).value();
+	}
+
+	bool CreateArtifact() {
+		string script = R"(#! /bin/sh
+
+DIRNAME=$(dirname $0)
+
+# Create small tar file
+echo foobar > ${DIRNAME}/testdata
+mender-artifact \
+    --compression none \
+    write rootfs-image \
+    --no-progress \
+    -t test-device \
+    -n test-artifact \
+    -f ${DIRNAME}/testdata \
+    -o ${DIRNAME}/artifact.mender || exit 1
+
+exit 0
+		)";
+
+		const string script_fname = path::Join(temp_dir.Path(), "/test-script.sh");
+
+		std::ofstream os(script_fname.c_str(), std::ios::out);
+		os << script;
+		os.close();
+
+		int ret = chmod(script_fname.c_str(), S_IRUSR | S_IWUSR | S_IXUSR);
+		if (ret != 0) {
+			return ret;
+		}
+
+		processes::Process proc({script_fname});
+		auto ex_line_data = proc.GenerateLineData();
+		if (!ex_line_data) {
+			return false;
+		}
+		EXPECT_EQ(proc.GetExitStatus(), 0) << "error message: " + ex_line_data.error().message;
+		return true;
+	}
+
+protected:
+	TemporaryDirectory test_state_dir;
+	TemporaryDirectory test_tree_dir;
+	TemporaryDirectory temp_dir;
+
+	conf::MenderConfig cfg {};
+	shared_ptr<context::MenderContext> ctx;
+	mender::artifact::PayloadHeader update_payload_header;
+};
+
+TEST_F(UpdateModuleFileTreeTests, FileTreeTestHeader) {
+	update_module::UpdateModule up_mod(*ctx, update_payload_header);
+	const string tree_path = test_tree_dir.Path();
+	auto err = up_mod.PrepareFileTree(tree_path);
+	ASSERT_EQ(err, error::NoError);
+
+	//
+	// Current device contents
+	//
+
+	EXPECT_TRUE(FileContains(path::Join(tree_path, "version"), "3\n"));
+
+	EXPECT_TRUE(FileContains(
+		path::Join(tree_path, "current_artifact_name"), "artifact-name existing-artifact-name\n"));
+
+	EXPECT_TRUE(FileContains(
+		path::Join(tree_path, "current_artifact_group"),
+		"artifact-group existing-artifact-group\n"));
+
+	EXPECT_TRUE(FileContains(path::Join(tree_path, "current_device_type"), "Some device type\n"));
+
+	//
+	// Header contents (From the Artifact)
+	//
+
+	EXPECT_TRUE(FileContains(path::Join(tree_path, "header", "artifact_group"), ""));
+
+	EXPECT_TRUE(FileContains(path::Join(tree_path, "header", "artifact_name"), "test-artifact"));
+
+	EXPECT_TRUE(FileContains(path::Join(tree_path, "header", "payload_type"), "rootfs-image"));
+
+	string expected_header_info = R"(
+	{
+	  "artifact_depends": {
+	    "device_type": [
+	      "test-device"
+	    ]
+	  },
+	  "artifact_provides": {
+	    "artifact_name": "test-artifact"
+	  },
+	  "payloads": [
+	    {
+	      "type": "rootfs-image"
+	    }
+	  ]
+	}
+	)";
+	EXPECT_TRUE(
+		FileJsonEquals(path::Join(tree_path, "header", "header_info"), expected_header_info));
+
+
+	string expected_type_info = R"(
+	{
+	  "artifact_provides": {
+	    "rootfs-image.checksum":
+	    "aec070645fe53ee3b3763059376134f058cc337247c978add178b6ccdfb0019f",
+	    "rootfs-image.version": "test-artifact"
+	  },
+	  "clears_artifact_provides": [
+	    "artifact_group",
+	    "rootfs_image_checksum",
+	    "rootfs-image.*"
+	  ],
+	  "type": ""
+	})";
+	EXPECT_TRUE(FileJsonEquals(path::Join(tree_path, "header", "type_info"), expected_type_info));
+
+	// TODO
+	// EXPECT_TRUE(FileContains(path::Join(tree_path, "header", "meta_data"), "bar"));
+
+	err = up_mod.DeleteFileTree(tree_path);
+	ASSERT_EQ(err, error::NoError);
 }
