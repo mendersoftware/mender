3a2775b5642c04a42bccdfb7ae6f8f486cd35437
Kristian Amlie <kristian.amlie@northern.tech>
2023-07-13 10:21:06 +0200

    chore: Fix inclusion order.
    Signed-off-by: Kristian Amlie <kristian.amlie@northern.tech>

--- artifact/tar/platform/libarchive/wrapper.cpp
+++ artifact/tar/platform/libarchive/wrapper.cpp
@@ -12,9 +12,10 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 
+#include <libarchive/wrapper.hpp>
+
 #include <archive.h>
 
-#include <libarchive/wrapper.hpp>
 #include <common/log.hpp>
 
 namespace mender {
