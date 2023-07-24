832e10c6b9846014d25005c3a12c10322be59b18
Peter Grzybowski <peter@northern.tech>
2023-07-24 22:42:04 +0200

    chore: go fmt
    Ticket: None
    Signed-off-by: Peter Grzybowski <peter@northern.tech>
    (cherry picked from commit accec86da779253f2545d58b090af9e28ce42e75)

--- installer/dual_rootfs_device.go
+++ installer/dual_rootfs_device.go
@@ -1,16 +1,16 @@
 // Copyright 2023 Northern.tech AS
 //
-//    Licensed under the Apache License, Version 2.0 (the "License");
-//    you may not use this file except in compliance with the License.
-//    You may obtain a copy of the License at
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
 //
-//        http://www.apache.org/licenses/LICENSE-2.0
+//	    http://www.apache.org/licenses/LICENSE-2.0
 //
-//    Unless required by applicable law or agreed to in writing, software
-//    distributed under the License is distributed on an "AS IS" BASIS,
-//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//    See the License for the specific language governing permissions and
-//    limitations under the License.
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
 package installer
 
 import (
@@ -56,7 +56,8 @@ type DualRootfsDevice interface {
 // checkMounted parses /proc/self/mounts to check
 // if device partition @part is a mounted fileststem.
 // return: The mount target if partition is mounted
-//         else an empty string is returned
+//
+//	else an empty string is returned
 func checkMounted(part string) string {
 	file, err := os.Open("/proc/self/mounts")
 	if err != nil {
