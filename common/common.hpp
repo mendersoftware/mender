f40dfb8cf386e9395417d51902c389832679b707
Kristian Amlie <kristian.amlie@northern.tech>
2023-07-25 16:23:40 +0200

    chore: Add conditionally available type name, for debugging purposes.
    Signed-off-by: Kristian Amlie <kristian.amlie@northern.tech>

--- common/common.hpp
+++ common/common.hpp
@@ -22,6 +22,16 @@
 #include <string>
 #include <vector>
 
+#ifndef __clang__
+#define __has_feature(x) false
+#endif
+
+// GCC and Clang method, respectively.
+#if defined(__GXX_RTTI) || __has_feature(cxx_rtti)
+#define __MENDER_RTTI_AVAILABLE
+#include <typeinfo>
+#endif
+
 namespace mender {
 namespace common {
 
@@ -48,6 +58,15 @@ mender::common::expected::ExpectedLongLong StringToLongLong(const string &str, i
 
 vector<string> SplitString(const string &str, const string &delim);
 string JoinStrings(const vector<string> &str, const string &delim);
+
+template <typename T>
+string BestAvailableTypeName(const T &object) {
+#ifdef __MENDER_RTTI_AVAILABLE
+	return typeid(object).name();
+#else
+	return "<Type name not available>";
+#endif
+}
 
 } // namespace common
 } // namespace mender
