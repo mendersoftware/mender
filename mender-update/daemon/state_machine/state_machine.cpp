beb64caf6eaf0e9b4ab15c013ec12cab7caefb02
Kristian Amlie <kristian.amlie@northern.tech>
2023-10-03 14:31:19 +0200

    chore: Move main include to top.
    Signed-off-by: Kristian Amlie <kristian.amlie@northern.tech>

--- mender-update/daemon/state_machine/state_machine.cpp
+++ mender-update/daemon/state_machine/state_machine.cpp
@@ -12,11 +12,12 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 
+#include <mender-update/daemon/state_machine.hpp>
+
 #include <common/conf.hpp>
 #include <common/log.hpp>
 
 #include <mender-update/daemon/states.hpp>
-#include <mender-update/daemon/state_machine.hpp>
 
 namespace mender {
 namespace update {
