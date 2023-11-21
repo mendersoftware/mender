8783fdb81dc0c48527631c704cf8d807dcd91889
Kristian Amlie <kristian.amlie@northern.tech>
2023-11-21 10:34:45 +0100

    chore: Fix assert when device is unauthorized.
    Ticket: MEN-6856
    
    Signed-off-by: Kristian Amlie <kristian.amlie@northern.tech>

--- src/api/client_test.cpp
+++ src/api/client_test.cpp
@@ -536,7 +536,7 @@ TEST_F(APIClientTests, ClientEarlyAuthErrorTest) {
 	EXPECT_FALSE(body_handler_called);
 }
 
-TEST_F(APIClientTests, ClientReauthenticationFailureTest) {
+TEST_F(APIClientTests, ClientAuthenticationTimeoutFailureTest) {
 	const string JWT_TOKEN1 = "FOOBARJWTTOKEN1";
 	const string SERVER_URL = "http://127.0.0.1:" + TEST_PORT;
 	const string test_data1 = "some testing data 1";
@@ -633,6 +633,169 @@ TEST_F(APIClientTests, ClientReauthenticationFailureTest) {
 			header_handler_called2 = true;
 
 			EXPECT_FALSE(exp_resp);
+			EXPECT_EQ(exp_resp.error().code, auth::MakeError(auth::AuthenticationError, "").code);
+
+			// give the body handler a chance to run (it shouldn't, but if we do
+			// loop.Stop() here, it definitely won't)
+			timer.AsyncWait(chrono::seconds(1), [&loop](error::Error err) { loop.Stop(); });
+		};
+
+	http::ResponseHandler body_handler2 = [&body_handler_called2,
+										   &loop](http::ExpectedIncomingResponsePtr exp_resp) {
+		// should never be called
+		EXPECT_FALSE(body_handler_called2);
+		body_handler_called2 = true;
+		EXPECT_FALSE(exp_resp);
+		loop.Stop();
+	};
+
+	http::ResponseHandler header_handler1 =
+		[&header_handler_called1, received_body1](http::ExpectedIncomingResponsePtr exp_resp) {
+			EXPECT_FALSE(header_handler_called1);
+			header_handler_called1 = true;
+
+			auto body_writer = make_shared<io::ByteWriter>(received_body1);
+			ASSERT_TRUE(exp_resp);
+			auto resp = exp_resp.value();
+			auto content_length = resp->GetHeader("Content-Length");
+			auto ex_len = common::StringToLongLong(content_length.value());
+			ASSERT_TRUE(ex_len);
+			received_body1->resize(ex_len.value());
+			resp->SetBodyWriter(body_writer);
+		};
+
+	http::ResponseHandler body_handler1 = [&client,
+										   &body_handler_called1,
+										   test_data1,
+										   received_body1,
+										   req2,
+										   header_handler2,
+										   body_handler2,
+										   &loop](http::ExpectedIncomingResponsePtr exp_resp) {
+		EXPECT_FALSE(body_handler_called1);
+		body_handler_called1 = true;
+
+		EXPECT_EQ(common::StringFromByteVector(*received_body1), test_data1);
+		loop.Post([&client, req2, header_handler2, body_handler2]() {
+			auto err = client.AsyncCall(req2, header_handler2, body_handler2);
+			EXPECT_EQ(err, error::NoError) << "Unexpected error: " << err.message;
+		});
+	};
+
+	auto err = client.AsyncCall(req1, header_handler1, body_handler1);
+
+	EXPECT_EQ(err, error::NoError) << "Unexpected error: " << err.message;
+	loop.Run();
+
+	EXPECT_TRUE(header_handler_called1);
+	EXPECT_TRUE(body_handler_called1);
+	EXPECT_TRUE(header_handler_called2);
+	EXPECT_FALSE(body_handler_called2);
+}
+
+TEST_F(APIClientTests, ClientReauthenticationFailureTest) {
+	const string JWT_TOKEN1 = "FOOBARJWTTOKEN1";
+	const string SERVER_URL = "http://127.0.0.1:" + TEST_PORT;
+	const string test_data1 = "some testing data 1";
+	const string test_uri1 = "/test/uri/1";
+	const string test_uri2 = "/test/uri/2";
+
+	TestEventLoop loop;
+
+	// Setup a test server
+	http::ServerConfig server_config;
+	http::Server server(server_config, loop);
+	bool test_data1_sent = false;
+	bool test_data2_requested = false;
+	server.AsyncServeUrl(
+		SERVER_URL,
+		[JWT_TOKEN1, test_uri1, test_uri2, &test_data2_requested, &test_data1_sent](
+			http::ExpectedIncomingRequestPtr exp_req) {
+			ASSERT_TRUE(exp_req) << exp_req.error().String();
+			auto req = exp_req.value();
+			if (!test_data1_sent) {
+				EXPECT_EQ(req->GetPath(), test_uri1);
+				auto ex_auth = req->GetHeader("Authorization");
+				ASSERT_TRUE(ex_auth);
+				EXPECT_EQ(ex_auth.value(), "Bearer " + JWT_TOKEN1);
+			} else if (!test_data2_requested) {
+				EXPECT_EQ(req->GetPath(), test_uri2);
+				auto ex_auth = req->GetHeader("Authorization");
+				ASSERT_TRUE(ex_auth);
+				EXPECT_EQ(ex_auth.value(), "Bearer " + JWT_TOKEN1);
+			}
+			req->SetBodyWriter(make_shared<io::Discard>());
+		},
+		[JWT_TOKEN1, test_uri1, test_uri2, test_data1, &test_data1_sent, &test_data2_requested](
+			http::ExpectedIncomingRequestPtr exp_req) {
+			ASSERT_TRUE(exp_req) << exp_req.error().String();
+
+			auto result = exp_req.value()->MakeResponse();
+			ASSERT_TRUE(result);
+			auto resp = result.value();
+
+			auto req = exp_req.value();
+			if (!test_data1_sent) {
+				resp->SetStatusCodeAndMessage(200, "OK");
+				resp->SetBodyReader(make_shared<io::StringReader>(test_data1));
+				resp->SetHeader("Content-Length", to_string(test_data1.size()));
+				test_data1_sent = true;
+			} else if (test_data1_sent && !test_data2_requested) {
+				// simulating expired token when requested data the second time
+				EXPECT_EQ(req->GetPath(), test_uri2);
+				resp->SetStatusCodeAndMessage(401, "Unauthorized");
+				test_data2_requested = true;
+			}
+			resp->AsyncReply([](error::Error err) { ASSERT_EQ(error::NoError, err); });
+		});
+
+	// Setup fake mender-auth simply returning auth data
+	dbus::DBusServer dbus_server {loop, "io.mender.AuthenticationManager"};
+	auto dbus_obj = make_shared<dbus::DBusObject>("/io/mender/AuthenticationManager");
+	dbus_obj->AddMethodHandler<dbus::ExpectedStringPair>(
+		"io.mender.Authentication1", "GetJwtToken", [JWT_TOKEN1, SERVER_URL]() {
+			return dbus::StringPair {JWT_TOKEN1, SERVER_URL};
+		});
+	dbus_obj->AddMethodHandler<expected::ExpectedBool>(
+		"io.mender.Authentication1", "FetchJwtToken", [&dbus_server]() {
+			auto err = dbus_server.EmitSignal(
+				"/io/mender/AuthenticationManager",
+				"io.mender.Authentication1",
+				"JwtTokenStateChange",
+				dbus::StringPair {"", ""});
+			EXPECT_EQ(err, error::NoError);
+			return true;
+		});
+	dbus_server.AdvertiseObject(dbus_obj);
+
+	auth::Authenticator authenticator {loop, chrono::seconds {2}};
+
+	http::ClientConfig client_config {""};
+	api::HTTPClient client {client_config, loop, authenticator};
+
+	auto req1 = make_shared<api::APIRequest>();
+	req1->SetPath(test_uri1);
+	req1->SetMethod(http::Method::GET);
+
+	auto received_body1 = make_shared<vector<uint8_t>>();
+	bool header_handler_called1 = false;
+	bool body_handler_called1 = false;
+
+	auto req2 = make_shared<api::APIRequest>();
+	req2->SetPath(test_uri2);
+	req2->SetMethod(http::Method::GET);
+
+	bool header_handler_called2 = false;
+	bool body_handler_called2 = false;
+
+	events::Timer timer {loop};
+	http::ResponseHandler header_handler2 =
+		[&header_handler_called2, &timer, &loop](http::ExpectedIncomingResponsePtr exp_resp) {
+			EXPECT_FALSE(header_handler_called2);
+			header_handler_called2 = true;
+
+			EXPECT_FALSE(exp_resp);
+			EXPECT_EQ(exp_resp.error().code, auth::MakeError(auth::UnauthorizedError, "").code);
 
 			// give the body handler a chance to run (it shouldn't, but if we do
 			// loop.Stop() here, it definitely won't)
