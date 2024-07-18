aed8deba407e0d57d2cb904c8de08f341da373f9
Vratislav Podzimek <vratislav.podzimek@northern.tech>
2024-07-18 13:22:18 +0200

    feat: Add support for user:password@host in BreakDownUrl()
    And some basic unit tests for the function.
    
    Ticket: MEN-7402
    Changelog: none
    Signed-off-by: Vratislav Podzimek <vratislav.podzimek@northern.tech>
    (cherry picked from commit 3fb2a64957514b8421a30533898ba8d5c9362145)

--- src/common/http.hpp
+++ src/common/http.hpp
@@ -130,9 +130,11 @@ struct BrokenDownUrl {
 	string host;
 	int port {-1};
 	string path;
+	string username;
+	string password;
 };
 
-error::Error BreakDownUrl(const string &url, BrokenDownUrl &address);
+error::Error BreakDownUrl(const string &url, BrokenDownUrl &address, bool with_auth = false);
 
 string URLEncode(const string &value);
 
