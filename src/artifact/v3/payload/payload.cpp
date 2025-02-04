fc61ed8323f7bfefd2ba8ed441c0b75d611d20fa
John Olav Lund <john.olav.lund@northern.tech>
2025-02-04 08:40:05 +0100

    fix: Fix undefined behaviour after a move operation
    This fixes the case where we access an object after it has been moved.
    
    Ticket: MEN-7980
    Changelog: None
    
    Signed-off-by: John Olav Lund <john.olav.lund@northern.tech>

--- src/artifact/v3/payload/payload.cpp
+++ src/artifact/v3/payload/payload.cpp
@@ -48,7 +48,8 @@ ExpectedPayloadReader Payload::Next() {
 			parser_error::Code::ParseError, expected_tar_entry.error().message));
 	}
 	auto tar_entry {expected_tar_entry.value()};
-	return Reader {std::move(tar_entry), manifest_.Get("data/0000/" + tar_entry.Name())};
+	string tar_name = tar_entry.Name();
+	return Reader {std::move(tar_entry), manifest_.Get("data/0000/" + tar_name)};
 }
 
 } // namespace payload
