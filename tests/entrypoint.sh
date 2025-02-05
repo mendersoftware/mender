6b2adfe180316f3a340aef000ac00cde117919ab
John Olav Lund <john.olav.lund@northern.tech>
2025-02-05 09:51:41 +0100

    chore: Remove ssh configuration from the docker test container
    Ticket: MEN-7972
    Changelog: None
    
    Signed-off-by: John Olav Lund <john.olav.lund@northern.tech>
    (cherry picked from commit dab59a086acdb95348d326de47dffdc1f64d518a)

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -22,7 +22,6 @@ if [ -n "$TENANT_TOKEN" ]; then
     sed -i -e "s/\"TenantToken\": *\"[^\"]*\"/\"TenantToken\": \"$TENANT_TOKEN\"/" /etc/mender/mender.conf
 fi
 
-/etc/init.d/ssh start
 cp /usr/share/dbus-1/system.d/io.mender.AuthenticationManager.conf /etc/dbus-1/system-local.conf
 dbus-daemon --nofork --nopidfile --system &
 sleep 8
