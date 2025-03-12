cb45358d51cb39b131fc8f5253283faf01aa68d4
John Olav Lund <john.olav.lund@northern.tech>
2025-03-12 11:43:41 +0100

    chore: Log an error when the server certificate fails to load
    Ticket: MEN-7964
    Changelog: None
    
    Signed-off-by: John Olav Lund <john.olav.lund@northern.tech>

--- src/common/http/platform/beast/http.cpp
+++ src/common/http/platform/beast/http.cpp
@@ -356,7 +356,7 @@ error::Error Client::Initialize() {
 		if (client_config_.server_cert_path != "") {
 			ssl_ctx_[i].load_verify_file(client_config_.server_cert_path, ec);
 			if (ec) {
-				log::Warning("Failed to load the server certificate! Falling back to the CA store");
+				log::Error("Failed to load the server certificate! Falling back to the CA store");
 			}
 		}
 	}
