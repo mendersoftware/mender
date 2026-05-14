2af11152a8b0073610b655ab8311a7b12b3295dc
Vratislav Podzimek <vratislav.podzimek+auto-signed@northern.tech>
2026-05-14 17:34:18 +0200

    chore: Refactor the `dist-package` target into separate install components
    This way the extra pieces like the default config and
    `device_type` file can be installed even without running the
    `dist-package` target, for example from packaging recipes.
    
    Ticket: MEN-9650
    Changelog: none
    Signed-off-by: Vratislav Podzimek <vratislav.podzimek+auto-signed@northern.tech>

--- cmake/dist-package.cmake
+++ cmake/dist-package.cmake
@@ -15,12 +15,32 @@ string(TOLOWER "${CMAKE_SYSTEM_NAME}-${CMAKE_SYSTEM_PROCESSOR}" _MENDER_DEVICE_T
 set(MENDER_DEVICE_TYPE "${_MENDER_DEVICE_TYPE_DEFAULT}" CACHE STRING "device_type baked into the dist-package /var/lib/mender/device_type")
 option(MENDER_BUILD_DIST_PACKAGE "Build the dist-package tarball as part of the default build" OFF)
 
-function(_setup_dist_package)
-    # Clear potential stale configured files
-    file(REMOVE_RECURSE ${CMAKE_BINARY_DIR}/dist-package)
-    configure_file(${CMAKE_CURRENT_LIST_DIR}/dist-package/mender.conf.in ${CMAKE_BINARY_DIR}/dist-package/mender.conf FILE_PERMISSIONS OWNER_READ OWNER_WRITE @ONLY)
-    configure_file(${CMAKE_CURRENT_LIST_DIR}/dist-package/device_type.in ${CMAKE_BINARY_DIR}/dist-package/device_type @ONLY)
+# Clear potential stale configured files
+file(REMOVE_RECURSE ${CMAKE_BINARY_DIR}/dist-package)
+configure_file(${CMAKE_CURRENT_LIST_DIR}/dist-package/mender.conf.in ${CMAKE_BINARY_DIR}/dist-package/mender.conf FILE_PERMISSIONS OWNER_READ OWNER_WRITE @ONLY)
+configure_file(${CMAKE_CURRENT_LIST_DIR}/dist-package/device_type.in ${CMAKE_BINARY_DIR}/dist-package/device_type @ONLY)
 
+# /var/lib/mender
+set(MENDER_STATE_DIR ${CMAKE_INSTALL_LOCALSTATEDIR}/lib/mender)
+install(DIRECTORY
+  DESTINATION ${MENDER_STATE_DIR}
+  COMPONENT mender-state-dir
+  EXCLUDE_FROM_ALL
+)
+install(FILES
+  ${CMAKE_BINARY_DIR}/dist-package/mender.conf
+  DESTINATION ${CMAKE_INSTALL_SYSCONFDIR}/mender
+  COMPONENT mender-conf
+  EXCLUDE_FROM_ALL
+)
+install(FILES
+  ${CMAKE_BINARY_DIR}/dist-package/device_type
+  DESTINATION ${MENDER_STATE_DIR}
+  COMPONENT mender-device-type
+  EXCLUDE_FROM_ALL
+)
+
+function(_setup_dist_package)
     set(STAGE_DIR ${CMAKE_BINARY_DIR}/mender-${MENDER_VERSION})
 
     set(DIST_DEPS mender-update)
@@ -37,10 +57,9 @@ function(_setup_dist_package)
     add_custom_target(${_dp_target_args}
         COMMAND ${CMAKE_COMMAND} -E rm -rf ${STAGE_DIR}
         COMMAND ${CMAKE_COMMAND} -E env DESTDIR=${STAGE_DIR} ${CMAKE_COMMAND} --install ${CMAKE_BINARY_DIR} --prefix /usr
-        COMMAND ${CMAKE_COMMAND} -E make_directory ${STAGE_DIR}/etc/mender
-        COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_BINARY_DIR}/dist-package/mender.conf ${STAGE_DIR}/etc/mender/
-        COMMAND ${CMAKE_COMMAND} -E make_directory ${STAGE_DIR}/var/lib/mender
-        COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_BINARY_DIR}/dist-package/device_type ${STAGE_DIR}/var/lib/mender/
+        COMMAND ${CMAKE_COMMAND} -E env DESTDIR=${STAGE_DIR} ${CMAKE_COMMAND} --install ${CMAKE_BINARY_DIR} --prefix / --component mender-state-dir
+        COMMAND ${CMAKE_COMMAND} -E env DESTDIR=${STAGE_DIR} ${CMAKE_COMMAND} --install ${CMAKE_BINARY_DIR} --prefix / --component mender-conf
+        COMMAND ${CMAKE_COMMAND} -E env DESTDIR=${STAGE_DIR} ${CMAKE_COMMAND} --install ${CMAKE_BINARY_DIR} --prefix / --component mender-device-type
         COMMAND tar -czf mender-${MENDER_VERSION}.tar.gz --owner=0 --group=0 mender-${MENDER_VERSION}
         COMMAND ${CMAKE_COMMAND} -E rm -rf ${STAGE_DIR}
         COMMAND ${CMAKE_COMMAND} -E echo "Created ${CMAKE_BINARY_DIR}/mender-${MENDER_VERSION}.tar.gz"
