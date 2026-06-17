de5d6314dd6c781d19a9dae49146c6479581949e
Paweł Poławski <pawel.polawski@northern.tech>
2026-06-17 09:27:36 +0200

    chore(build): Make sure C++ build flags are applied to C++ files only
    Signed-off-by: Paweł Poławski <pawel.polawski@northern.tech>

--- cmake/build_mode.cmake
+++ cmake/build_mode.cmake
@@ -4,13 +4,13 @@ if ("${CMAKE_BUILD_TYPE}" MATCHES "Rel")
   # The ABI is not guaranteed to be compatible between C++11 and C++17, so in release mode, let's
   # not take that risk, and compile everything under C++17 instead. We still use C++11 in debug mode
   # below.
-  add_compile_options(-std=c++17)
+  add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-std=c++17>)
   add_compile_definitions(MENDER_CXX_STANDARD=17)
   # No need for it in release mode, we compile everything with the same options.
   set(PLATFORM_SPECIFIC_COMPILE_OPTIONS "")
 else()
   # In Debug mode use C++11 by default, so that we catch C++11 violations.
-  add_compile_options(-std=c++11)
+  add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-std=c++11>)
   add_compile_definitions(MENDER_CXX_STANDARD=11)
   # Use this with target_compile_options for platform specific components that need it.
   set(PLATFORM_SPECIFIC_COMPILE_OPTIONS -std=c++17)
